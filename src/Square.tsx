c5dbd837380510a8b2131e369eb46dbc782825e4
futugyou <17871902+futugyou@users.noreply.github.com>
2022-12-31 14:23:34 +0800

    use  vite to create react project
    use  vite to create react project

---
+++ src/Square.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Square() {
+    return (
+        <button className="square">
+            {/* TODO */}
+        </button>
+    )
+}
+
+export default Square
