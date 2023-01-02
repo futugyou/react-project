df7ae1bb03966bb144a588da2ce1d5e2ead53948
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-02 21:26:27 +0800

    upgrade component state
    upgrade component state

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,26 +1,52 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
+import TemperatureInput from './TemperatureInput'
 import BoilingVerdict from './BoilingVerdict'
+import { useState } from 'react'
+
+function toCelsius(fahrenheit: number) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius: number) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature: string, convert: (arg0: number) => any) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
 
 function Calculator(props: any) {
     const [state, setState] = useState(
         {
-            temperature: ''
+            temperature: '',
+            scale: 'c'
         })
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        var newData = Object.assign({}, state, { temperature: event.target.value });
+    const handleCelsiusChange = (temperature: number) => {
+        var newData = Object.assign({}, state, { scale: 'c', temperature: temperature });
         setState(newData);
     }
 
+    const handleFahrenheitChange = (temperature: number) => {
+        var newData = Object.assign({}, state, { scale: 'f', temperature: temperature });
+        setState(newData);
+    }
+
+    const scale = state.scale;
+    const temperature = state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
     return (
         <div className="card">
-            <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input
-                    value={state.temperature}
-                    onChange={(e) => handleChange(e)} />
-                <BoilingVerdict celsius={parseFloat(state.temperature)} />
-            </fieldset>
+            <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={handleCelsiusChange} />
+            <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={handleFahrenheitChange} />
+            <BoilingVerdict celsius={parseFloat(celsius)} />
         </div>
     )
 }
---
+++ src/TemperatureInput.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent } from 'react'
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+}
+
+function TemperatureInput(props: any) {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        props.onTemperatureChange(event.target.value);
+    }
+
+    const temperature = props.temperature;
+    const scale = props.scale;
+
+    return (
+        <div className="card">
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature} onChange={(e) => handleChange(e)} />
+            </fieldset>
+        </div>
+    )
+}
+
+export default TemperatureInput
