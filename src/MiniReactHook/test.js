f5d8c76bcc094331b9c65037a7ab7a7d15591400
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-23 16:56:07 +0800

    update hook demo
    update hook demo

--- src/MiniReactHook/test.js
+++ src/MiniReactHook/test.js
@@ -4,3 +4,39 @@ function test() {
     setCount(1)
     console.log(count());
 }
+
+function test1() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('count is:', count())
+    });
+    useEffect(() => {
+        console.log('nothing to do')
+    });
+    setCount(2)
+}
+
+function test2() {
+    const [name1, setName1] = useState('LiLei');
+    const [name2, setName2] = useState('hanmeimei');
+    const [showAll, setShowAll] = useState(true);
+
+    const whoishere = useMemo(() => {
+        if (!showAll) {
+            return name1();
+        }
+        return name1() + ' and ' + name2()
+    });
+
+    useEffect(() => {
+        console.log('who is herr: ', whoishere())
+    });
+
+    setName1('xiaoming');
+    setShowAll(false);
+
+    // no log print
+    setName2('xiaohong')
+
+}
