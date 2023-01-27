e8e66322216b6ae5652fb6edcc7fa3bbfc41b5f5
futugyou <17871902+futugyou@users.noreply.github.com>
2023-01-27 18:08:35 +0800

    custom react schedule step 2
    improve our scheduler to add priority,
    use getFirstCallbackNode and cancelCallback from 'Scheduler' package

--- src/MiniReactHook/scheduler.ts
+++ src/MiniReactHook/scheduler.ts
@@ -1,17 +1,65 @@
+import {
+    unstable_IdlePriority as IdlePriority,
+    unstable_ImmediatePriority as ImmediatePriority,
+    unstable_LowPriority as LowPriority,
+    unstable_NormalPriority as NormalPriority,
+    unstable_UserBlockingPriority as UserBlockingPriority,
+    unstable_getFirstCallbackNode as getFirstCallbackNode,
+    unstable_scheduleCallback as scheduleCallback,
+    unstable_cancelCallback as cancelCallback,
+} from "scheduler";
 
 const contentBox = document.querySelector("#content") as Element;
 
+type Priority =
+    | typeof ImmediatePriority
+    | typeof UserBlockingPriority
+    | typeof NormalPriority
+    | typeof LowPriority
+    | typeof IdlePriority;
+
 interface Work {
     count: number;
+    priority: Priority;
 }
+
 const workList: Work[] = [];
+const prevPriority = IdlePriority;
+let curCallback;
 
 function schedule() {
-    // 从队列末尾获取一个work
-    const currwork = workList.pop();
-    if (currwork) {
-        perform(currwork);
+    // // 从队列末尾获取一个work
+    // const currwork = workList.pop();
+
+    // 尝试获取当前调度的callback
+    const cbNode = getFirstCallbackNode();
+
+    // 获取优先级最高的work
+    const sortedWorkList = workList.sort((w1, w2) => {
+        return w1.priority - w2.priority;
+    });
+
+    const currwork = sortedWorkList[0];
+
+    // 没有要调度的work，return
+    if (!currwork) {
+        curCallback = null;
+        cbNode && cancelCallback(cbNode);
+        return
     }
+
+    // 获取currwork的优先级
+    const { priority: curPriority } = currwork;
+    // 如果和上次优先级相同，return
+    if (curPriority == prevPriority) {
+        return
+    }
+
+    // 如果有其他工作在进行，中断它
+    cbNode && cancelCallback(cbNode);
+
+    // 调度当前优先级最高的work
+    curCallback = scheduleCallback(curPriority, perform.bind(null, currwork));
 }
 
 function perform(work: Work) {
@@ -31,7 +79,8 @@ const insertItem = (content: string) => {
 const button = document.createElement("buttoon");
 button.onclick = () => {
     workList.unshift({
-        count: 100
+        count: 100,
+        priority: IdlePriority,
     });
     schedule();
 }
