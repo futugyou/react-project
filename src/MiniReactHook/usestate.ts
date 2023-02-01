6ef8015076855308bf13849d27869d4af97dffaf
futugyou <17871902+futugyou@users.noreply.github.com>
2023-02-01 14:58:08 +0800

    react chapter 8 custom useState
    add other base datastruct

--- src/MiniReactHook/usestate.ts
+++ src/MiniReactHook/usestate.ts
@@ -7,5 +7,29 @@ interface Update {
     next?: Update;
 }
 
+// update queue 单向环状链表
+interface Queue {
+    pending?: Update;
+}
 
+//
+interface Hook {
+    // 保存update的queue，即dispatchSetState接收的queue
+    queue: Queue;
+    // 保存hook对应的state
+    memoizedState: any;
+    // 与下一个hook连接形成单向无环链表
+    next?: Hook;
+}
+
+interface Fiber {
+    // 保存该FC对应的的Hooks链表
+    memoizedState?: Hook;
+    // 指向APP对象
+    stateNode: () => { click: () => void };
+}
+
+let callbackNode: number | undefined = undefined;
+let workInProgressHook: Hook | undefined;
+let isMount = true;
 export { }
