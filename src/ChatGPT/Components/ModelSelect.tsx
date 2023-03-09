87381c19cf974f37f3ca11b03cc3dd344b2af369
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-09 18:18:39 +0800

    add model select components
    add model select components

---
+++ src/ChatGPT/Components/ModelSelect.tsx
@@ -0,0 +1,31 @@
+import './DefaultQA.css'
+import Form from 'react-bootstrap/Form';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+
+function ModelSelect(props: any) {
+    const modelPopover = (
+        <Popover id="model-popover">
+            <Popover.Body>
+                The model which will generate the completion. Some models are suitable for natural language tasks, others specialize in code. <a href='https://platform.openai.com/docs/models'>Learn more</a>.
+            </Popover.Body>
+        </Popover>
+    );
+
+    return (
+        <>
+            <OverlayTrigger placement="left" overlay={modelPopover} delay={{ show: 100, hide: 3000 }} >
+                <Form.Group className="mb-3" >
+                    <Form.Label>Model</Form.Label>
+                    <Form.Select value={props.model} onChange={e => props.handleModelChange(e.target.value)} >
+                        <option value="text-davinci-003">text-davinci-003</option>
+                        <option value="text-davinci-002">text-davinci-002</option>
+                        <option value="text-davinci-001">text-davinci-001</option>
+                    </Form.Select>
+                </Form.Group>
+            </OverlayTrigger>
+        </>
+    )
+}
+
+export default ModelSelect
