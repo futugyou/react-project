bacc0cabd71e265793e439e1dc045a2ad2137b66
futugyou <17871902+futugyou@users.noreply.github.com>
2023-03-11 15:41:34 +0800

    update model component
    update model component

--- src/ChatGPT/Components/ModelSelect.tsx
+++ src/ChatGPT/Components/ModelSelect.tsx
@@ -5,12 +5,22 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Popover from 'react-bootstrap/Popover';
 import ModelService from '../Services/Model';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function ModelSelect(props: any) {
     const [model, setModel] = useState(props.model)
     const [models, setModels] = useState([])
 
-    const modelPopover = (
+    const modelPopover = (display: string) => (
+        <Popover id="model-popover">
+            <Popover.Body>
+                {display}
+            </Popover.Body>
+        </Popover>
+    );
+
+    const modelDescriptPopover = (
         <Popover id="model-popover">
             <Popover.Body>
                 The model which will generate the completion. Some models are suitable for natural language tasks, others specialize in code. <a href='https://platform.openai.com/docs/models'>Learn more</a>.
@@ -33,22 +43,34 @@ function ModelSelect(props: any) {
 
     const dropdown_options = models.map((item, index) => {
         return (
-            <Dropdown.Item eventKey={item} key={item}>
-                {item}
-            </Dropdown.Item>
+            <OverlayTrigger placement="left" overlay={modelPopover(item)}>
+                <Dropdown.Item eventKey={item} key={item}>
+                    {item}
+                </Dropdown.Item>
+            </OverlayTrigger>
         );
     });
     return (
         <>
             <Form.Group className="mb-3" >
-                <Dropdown onSelect={onModelChange}>
-                    <OverlayTrigger placement="left" overlay={modelPopover}>
-                        <Dropdown.Toggle variant="select" id="dropdown-basic">
-                            {model}
-                        </Dropdown.Toggle>
-                    </OverlayTrigger>
-                    <Dropdown.Menu >{dropdown_options}</Dropdown.Menu>
-                </Dropdown>
+                <Row>
+                    <Col>
+                        <OverlayTrigger placement="left" overlay={modelDescriptPopover}>
+                            <Form.Label>Model</Form.Label>
+                        </OverlayTrigger>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Dropdown onSelect={onModelChange}>
+                            <Dropdown.Toggle variant="select" id="dropdown-basic">
+                                {model}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu >{dropdown_options}</Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                </Row>
+
             </Form.Group>
         </>
     )
