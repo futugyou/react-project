edf6290cac6e059a40be5ab789e34c6775ba3b99
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-02 19:22:57 +0800

    react,add tip for stop component
    react,add tip for stop component

--- src/OpenAI/Components/Stop.tsx
+++ src/OpenAI/Components/Stop.tsx
@@ -1,5 +1,6 @@
 
 import './Stop.css'
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
@@ -7,21 +8,38 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Stop(props: any) {
+    const [state, setState] = useState('');
+    const [tip, setTip] = useState('');
+    const [show, setShow] = useState(false);
     let display = "Stop sequences"
     let subDisplay = "Enter sequence and press Tab"
     let popover = "Up to four sequences where the API will stop generating further tokens.The returned text will not contain the stop sequence."
 
-    const modelPopover = (
-        <Popover id="model-popover">
+    const stopPopover = (
+        <Popover id="stop-popover">
             <Popover.Body>
                 {popover}
             </Popover.Body>
         </Popover>
     );
+
+    const openTip = () => {
+        setShow(!show);
+    }
+
+    const onStopChange = (value: string) => {
+        setState(value)
+        if (show) {
+            setTip("Add \"" + value + "\"")
+        } else {
+            setTip('')
+        }
+    }
+
     return (
         <>
-            <OverlayTrigger placement="left" overlay={modelPopover}>
-                <Form.Group className="mb-3" >
+            <OverlayTrigger placement="left" overlay={stopPopover}>
+                <Form.Group className="mb-3 stop-container" >
                     <Row>
                         <Col>
                             <Form.Label>{display}</Form.Label>
@@ -36,9 +54,18 @@ function Stop(props: any) {
                     )}
                     <Row>
                         <Col>
-                            <Form.Control type="text" onChange={e => props.onStopChange(e.target.value)} />
+                            <Form.Control type="text" onChange={e => onStopChange(e.target.value)} onClick={e => openTip()} value={state} />
                         </Col>
                     </Row>
+                    {show && (
+                        <>
+                            <Row className='disabled-input' >
+                                <Col>
+                                    <Form.Control type="text" disabled placeholder="Enter a sequence" value={tip} />
+                                </Col>
+                            </Row>
+                        </>
+                    )}
                 </Form.Group>
             </OverlayTrigger>
         </>
