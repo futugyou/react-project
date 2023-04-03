a28b295cc75bd01a124ccaa7d6830f54a5beafa5
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-03 19:42:27 +0800

    react, add onclick event for close
    react, add onclick event for close

--- src/OpenAI/Components/Stop.tsx
+++ src/OpenAI/Components/Stop.tsx
@@ -23,7 +23,7 @@ function Stop(props: any) {
             <div key={item} className="stop-multiValue">
                 <div className="stop-item">  {item == '\n' ? '↵' : item}</div>
 
-                <div className="stop-close">
+                <div className="stop-close" onClick={e => removeStop(item)} >
                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="14px" width="14px" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M8 8.707l3.646 3.647.708-.707L8.707 8l3.647-3.646-.707-.708L8 7.293 4.354 3.646l-.707.708L7.293 8l-3.646 3.646.707.708L8 8.707z"></path></svg>
                 </div>
@@ -40,6 +40,17 @@ function Stop(props: any) {
 
     const openTip = () => {
         setShow(!show);
+    }
+
+    const removeStop = (value: string) => {
+        let s = stop
+        s.delete(value)
+        setStop(s)
+    }
+
+    const removeAllStop = () => {
+        let s = new Set<string>();
+        setStop(s)
     }
 
     const onStopChange = (value: string) => {
@@ -91,7 +102,7 @@ function Stop(props: any) {
                         </div>
                         {stop.size > 0 && (
                             <div className='closs-container'>
-                                <div className='closs-indicatorContainer'>
+                                <div className='closs-indicatorContainer' onClick={e => removeAllStop()} >
                                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">
                                         <path fillRule="evenodd" clipRule="evenodd" d="M8 8.707l3.646 3.647.708-.707L8.707 8l3.647-3.646-.707-.708L8 7.293 4.354 3.646l-.707.708L7.293 8l-3.646 3.646.707.708L8 8.707z"></path>
                                     </svg>
