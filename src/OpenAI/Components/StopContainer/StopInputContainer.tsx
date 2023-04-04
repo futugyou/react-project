9132c4aecb59e9fd36d59b9c9377442e8ea5fc84
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-04 19:20:13 +0800

    react, split stop clean all component
    react, split stop clean all component

--- src/OpenAI/Components/StopContainer/StopInputContainer.tsx
+++ src/OpenAI/Components/StopContainer/StopInputContainer.tsx
@@ -1,5 +1,6 @@
-import './StopInputContainer.css'
+import './StopInputContainer.css';
 import ExistedStop from './ExistedStop';
+import CleanAllStop from './CleanAllStop';
 
 function StopInputContainer(props: any) {
     const existedStops: any[] = [];
@@ -23,14 +24,8 @@ function StopInputContainer(props: any) {
                     </div>
                 </div>
             </div>
-            {props.stop.size > 0 && (
-                <div className='close-all-container'>
-                    <div className='closs-indicatorContainer' onClick={e => props.removeAllStop()} >
-                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M8 8.707l3.646 3.647.708-.707L8.707 8l3.647-3.646-.707-.708L8 7.293 4.354 3.646l-.707.708L7.293 8l-3.646 3.646.707.708L8 8.707z"></path>
-                        </svg>
-                    </div>
-                </div>)}
+
+            <CleanAllStop stop={props.stop} removeAllStop={() => props.removeAllStop()}></CleanAllStop>
         </div>
     )
 }
