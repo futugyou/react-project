5a6d3f5579e6c0a5aeea555fe7f9949db1887f62
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-11 22:24:40 +0800

    react, add sse method fro completion
    react, add sse method fro completion

--- src/OpenAI/Services/Completion.ts
+++ src/OpenAI/Services/Completion.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { openaiserver } from './Const';
 import { OpenAIModel } from '../Models/OpenAIModel';
 import { CompletionModel, DefaultCompletionModel } from '../Models/CompletionModel';
+import * as SSE from 'sse.js';
 
 const createCompletionPath = 'qa/'
 
@@ -31,6 +32,25 @@ const createCompletion = async (data: OpenAIModel) => {
     return result;
 }
 
+const createCompletionSSE = async (data: OpenAIModel) => {
+    const path = 'completions/'
+    const sse = new SSE();
+    sse.connect(`${openaiserver}${path}`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+        payload: JSON.stringify(data),
+    });
+
+    sse.addEventListener('message', (event: any) => {
+        console.log(event)
+    });
+
+    sse.stream();
+}
+
 export default {
     createCompletion: createCompletion,
+    createCompletionSSE: createCompletionSSE,
 }
