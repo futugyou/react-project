49621cfd751ee9f1c577154833b2da0453463ed6
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-17 22:55:08 +0800

    react, auto reset textarea height
    react, auto reset textarea height

--- src/OpenAI/Components/ChatPanel.tsx
+++ src/OpenAI/Components/ChatPanel.tsx
@@ -5,16 +5,26 @@ import Form from 'react-bootstrap/Form';
 import { BsDashCircle } from "react-icons/bs";
 
 function ChatPanel(props: any) {
-    const chatpgmessageRef = useRef(null);
-    const textRef = useRef(null);
-    const HandleTextClick = () => {
+    const chatpgmessageRef = useRef<HTMLDivElement>(null);
+    const textRef = useRef<HTMLTextAreaElement>(null);
+
+    const HandleTextDivClick = () => {
         chatpgmessageRef.current!.className = "chat-pg-message active"
         textRef.current!.focus();
+    }
+
+    const HandleTextChange = (e: { target: { value: string; }; }) => {
+        textRef.current!.style.height = "48px";
+        let height = textRef.current!.scrollHeight
+        textRef.current!.style.height = (height) + "px";
+
+        let text: string = e.target.value;
     }
 
     const HandleTextBlur = () => {
         chatpgmessageRef.current!.className = "chat-pg-message"
     }
+
     return (
         <div className="container-fluid chat-pg-body">
             <div className='chat-pg-instructions'>
@@ -31,9 +41,10 @@ function ChatPanel(props: any) {
                                 <span className='chat-message-role-text'>User</span>
                             </div>
                         </div>
-                        <div className='text-input-with-focus' onClick={HandleTextClick}>
+                        <div className='text-input-with-focus' onClick={HandleTextDivClick}>
                             <textarea className="text-input" rows={1} placeholder="Enter a user message here."
                                 ref={textRef}
+                                onChange={HandleTextChange}
                                 onBlur={HandleTextBlur}></textarea>
                         </div>
                         <div className='chat-message-button-container'>
