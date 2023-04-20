b3cf1c8fa0f3c0fb6e599a49d680a2f57b0eb381
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-20 20:38:39 +0800

    react, hide/show sidebar menu
    react, hide/show sidebar menu

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,57 +1,73 @@
+import { useState } from 'react';
 import { Outlet, NavLink, useNavigation } from "react-router-dom";
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
+import { BsListUl } from "react-icons/bs";
+
 function Layout() {
     const navigation = useNavigation();
+    const [show, setShow] = useState(false);
+
+    const HandleShowIconClick = (e: any) => {
+        setShow(s => !s)
+    }
 
     return (
         <>
+            <div className='sidebar-menu-icon' onClick={HandleShowIconClick}>
+                <BsListUl />
+            </div>
             <Row className="header">
                 <Col>
                     <h5>Header</h5>
                 </Col>
             </Row>
             <Row className="flex-grow-1 flex-fill">
-                <Col xs={2}>
-                    <ul className="nav nav-pills flex-column mb-auto">
-                        <li className="nav-item">
-                            <NavLink to={`app`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >App</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`playground`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >OpenAI Playground</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`game`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Game</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`from`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >NameForm</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`calculator`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Calculator</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`dialog`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >WelcomeDialog</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`split`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >SplitPaneApp</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`bailout`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Bailout</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`withbailout`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >WithoutBailout</NavLink>
-                        </li>
-                    </ul>
-                </Col>
-                <Col xs={10} className="d-flex flex-column">
+                {show && (
+                    <Col xs={2}>
+                        <ul className="nav nav-pills flex-column mb-auto">
+                            <li className="nav-item">
+                                <NavLink to={`app`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >App</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`playground`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >OpenAI Playground</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`game`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Game</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`from`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >NameForm</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`calculator`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Calculator</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`dialog`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >WelcomeDialog</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`split`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >SplitPaneApp</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`bailout`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Bailout</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={`withbailout`} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >WithoutBailout</NavLink>
+                            </li>
+                        </ul>
+                    </Col>
+                )}
+                <Col xs={show ? 10 : 12} className="d-flex flex-column">
                     <Row >
-                        <Breadcrumb>
-                            <Breadcrumb.Item href="/">App</Breadcrumb.Item>
-                            <Breadcrumb.Item href="/Game">Game</Breadcrumb.Item>
-                            <Breadcrumb.Item active>Data</Breadcrumb.Item>
-                        </Breadcrumb>
+                        <div>
+                            <Breadcrumb>
+                                <Breadcrumb.Item href="/">App</Breadcrumb.Item>
+                                <Breadcrumb.Item href="/Game">Game</Breadcrumb.Item>
+                                <Breadcrumb.Item active>Data</Breadcrumb.Item>
+                            </Breadcrumb>
+                        </div>
                     </Row>
                     <Row className={navigation.state === "loading" ? "flex-grow-1 flex-fill loading" : "flex-grow-1 flex-fill"}>
                         <Outlet />
