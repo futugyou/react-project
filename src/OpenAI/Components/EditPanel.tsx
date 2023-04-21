30908278256afaaeaabde2e7623637f54dc7b974
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-21 20:20:41 +0800

    react, update insert component
    react, update insert component

--- src/OpenAI/Components/EditPanel.tsx
+++ src/OpenAI/Components/EditPanel.tsx
@@ -3,27 +3,25 @@ import Form from 'react-bootstrap/Form';
 
 function EditPanel(props: any) {
     return (
-        <div className="container-fluid chat-pg-body chat-flex-direction">
-            <div className="chat-text-tip">
+        <div className="container-fluid edit-pg-body">
+            <div className="edit-text-tip">
                 <span>Input</span>
             </div>
-            <div className="chat-input-container">
-                <div className="chat-input-container-left">
-                    <div className="chat-input-container-input">
+            <div className="edit-input-container">
+                <div className="edit-input-container-left">
+                    <div className="edit-input-container-input">
                         <Form.Control as="textarea" rows={1} placeholder="We is going to the market." />
                     </div>
-                    <div className="chat-text-tip">
+                    <div className="edit-text-tip">
                         <span>Instructions</span>
                     </div>
                     <div>
                         <Form.Control as="textarea" rows={2} placeholder="Fix the grammar." />
                     </div>
                 </div>
-                <div className="chat-input-container-right">
+                <div className="edit-input-container-right">
                 </div>
             </div>
-
-
         </div>
     )
 }
--- src/OpenAI/Components/InsertPanel.tsx
+++ src/OpenAI/Components/InsertPanel.tsx
@@ -1,9 +1,37 @@
 import './InsertPanel.css'
 
+import { useState } from "react";
+
+import Form from 'react-bootstrap/Form';
+
 function InsertPanel(props: any) {
+    const [showPlaceholder, setShowPlaceholder] = useState(true);
+    const [text, setText] = useState(props.text ?? '')
+
+    const HandleTextChange = (e: any) => {
+        const t: string = e.target.value;
+        if (t.length > 0) {
+            setShowPlaceholder(false)
+        } else {
+            setShowPlaceholder(true)
+        }
+
+        if (props.onPromptChange) {
+            props.onPromptChange(t)
+        }
+    }
+
     return (
-        <div className="container-fluid chat-pg-body">
-            <h1>this is insert panel</h1>
+        <div className="container-fluid insert-pg-body">
+            <div className="insert-container-left">
+                <Form.Control as="textarea" rows={1} onChange={HandleTextChange} />
+                {showPlaceholder && (<div className="insert-placeholder">
+                    <div className="insert-placeholder-lg">We're writing to [insert]. Congrats from OpenAI!</div>
+                    <div className="insert-placeholder-md">Use [insert] to indicate where the model should insert text.</div>
+                </div>)}
+            </div>
+            <div className="insert-container-right">
+            </div>
         </div>
     )
 }
