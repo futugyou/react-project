b520d8a1df7dc321c59209a0f642b5a6f75dc2fb
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-23 20:24:07 +0800

    react, chat sse client
    react, chat sse client

---
+++ src/OpenAI/Services/Chat.ts
@@ -0,0 +1,31 @@
+import { openaiserver } from './Const';
+import { ChatModel } from '../Models/ChatModel';
+import * as SSEClient from '../../modules/sse';
+
+
+const createChatStream = async (data: ChatModel, processfn: (a: any) => void, endfn: () => void) => {
+    const path = 'chat/sse'
+    const sse = SSEClient.SSE(`${openaiserver}${path}`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+        payload: JSON.stringify(data),
+    });
+
+    sse.addEventListener('message', (event: any) => {
+        if (event.data == "[DONE]") {
+            sse.close();
+            endfn();
+        } else {
+            let tmp = event.data.replace(/\+/gi, '%20')
+            processfn(decodeURIComponent(tmp))
+        }
+    });
+
+    sse.stream();
+}
+
+export default {
+    createChatStream: createChatStream,
+}
