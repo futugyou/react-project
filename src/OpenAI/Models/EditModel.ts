48ab123ba6d06cf3901764af91db213e1fe3191b
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-25 20:59:50 +0800

    react, add edit service
    react, add edit service

---
+++ src/OpenAI/Models/EditModel.ts
@@ -0,0 +1,8 @@
+export interface EditModel {
+    model: string
+    input: string
+    instruction: string
+    temperature: number
+    n: number
+    top_p: number
+}
---
+++ src/OpenAI/Services/Edit.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { openaiserver } from './Const';
+import { EditModel } from '../Models/EditModel';
+import { CompletionModel, DefaultCompletionModel } from '../Models/CompletionModel';
+
+const createEdit = async (data: EditModel) => {
+    const path = 'edits/'
+    const options: AxiosRequestConfig = {
+        url: `${openaiserver}${path}`,
+        method: "POST",
+        data: data,
+    };
+
+    let result: CompletionModel = DefaultCompletionModel;
+
+    try {
+        const { data, status } = await axios<CompletionModel>(options);
+        result = {
+            ...DefaultCompletionModel,
+            ...data,
+        }
+    } catch (error: any) {
+        console.log(error);
+        result = {
+            ...result,
+            error: error.message
+        }
+    }
+
+    return result;
+}
+
+export default {
+    createEdit: createEdit,
+}
