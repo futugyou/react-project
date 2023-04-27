c7f033f77d9a8e2aeee90e6e02d60b18b0279b34
futugyou <17871902+futugyou@users.noreply.github.com>
2023-04-27 21:26:42 +0800

    react, create example component
    react, create example component

--- src/OpenAI/Components/Examples.tsx
+++ src/OpenAI/Components/Examples.tsx
@@ -1,7 +1,25 @@
+import './Examples.css'
+
 function Examples(props: any) {
     return (
-        <>
-        </>
+        <div className="example-page">
+            <div className="example-container">
+                <div className="example-header">
+                    <h1 className="text-title">Examples</h1>
+                    <h3 className="text-tagline">Explore what's possible with some example applications</h3>
+                </div>
+                <div className="example-search">
+                    <div className="example-search-input-container">
+                        <input className="example-search-input"></input>
+                    </div>
+                    <div className="example-search-categories-container">
+                        <select className="example-search-categories"></select>
+                    </div>
+                </div>
+                <div className="example-item-container">
+                </div>
+            </div>
+        </div>
     )
 }
 
