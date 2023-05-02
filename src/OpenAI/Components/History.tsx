61236c77731ea56bea45a2b50f9c0c4bb4942763
futugyou <17871902+futugyou@users.noreply.github.com>
2023-05-02 17:29:30 +0800

    react, add selected item style
    react, add selected item style

--- src/OpenAI/Components/History.tsx
+++ src/OpenAI/Components/History.tsx
@@ -1,38 +1,40 @@
 import './Histort.css'
 
-import { useState, Fragment } from 'react';
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { BsClockHistory } from "react-icons/bs";
+import { useState, Fragment } from 'react'
+import Button from 'react-bootstrap/Button'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import { BsClockHistory } from "react-icons/bs"
 
-import { PlaygroundModel } from '../Models/PlaygroundModel';
-import playgroundService from '../Services/Playground';
-import moment from 'moment';
+import { PlaygroundModel } from '../Models/PlaygroundModel'
+import playgroundService from '../Services/Playground'
+import moment from 'moment'
 
 function History(props: any) {
-    const [show, setShow] = useState(false);
-    const [historyList, setHistoryList] = useState<PlaygroundModel[]>([]);
+    const [show, setShow] = useState(false)
+    const [historyList, setHistoryList] = useState<PlaygroundModel[]>([])
+    const [historySelected, setHistorySelected] = useState(-1)
 
     const formatDate = (t: number) => {
-        var day = moment(t);
+        var day = moment(t)
         return day.format('LL dddd')
     }
 
     const formatTime = (t: number) => {
-        var day = moment(t);
+        var day = moment(t)
         return day.format('HH:mm')
     }
 
-    let lastDate = '';
+    let lastDate = ''
 
     const handleRecordClick = (data: PlaygroundModel) => {
+        setHistorySelected(data.createdAt)
         if (props.onHistoryRecordClick) {
             props.onHistoryRecordClick(data)
         }
     }
 
     const historyItems = historyList.map((data, index) => {
-        let currectDate = data.Date as string;
+        let currectDate = data.Date as string
         let dayDisplay = <></>
 
         if (lastDate != currectDate) {
@@ -44,21 +46,21 @@ function History(props: any) {
         return (
             <Fragment key={index}>
                 {dayDisplay}
-                <li  className="history-item-wrap" onClick={() => handleRecordClick(data)}>
+                <li className={historySelected === data.createdAt ? "history-item-wrap history-selected" : "history-item-wrap"} onClick={() => handleRecordClick(data)}>
                     <div className="history-item">
                         <div className="history-item-time">{formatTime(data.createdAt)}</div>
                         <div className="history-item-prompt"> {data.prompt}</div>
                     </div>
                 </li>
             </Fragment>
-        );
-    });
+        )
+    })
 
-    const handleClose = () => setShow(false);
+    const handleClose = () => setShow(false)
     const handleShow = () => {
-        const data = playgroundService.getPlayground();
+        const data = playgroundService.getPlayground()
         setHistoryList(data)
-        setShow(true);
+        setShow(true)
     }
 
     return (
@@ -79,7 +81,7 @@ function History(props: any) {
                 </Offcanvas.Body>
             </Offcanvas >
         </>
-    );
+    )
 }
 
-export default History;
+export default History
