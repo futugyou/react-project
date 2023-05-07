87e50fc1bce477ed51c7a33056657fec2990c364
futugyou <17871902+futugyou@users.noreply.github.com>
2023-05-07 18:10:17 +0800

    react, create example
    react, create example

--- src/OpenAI/Services/Example.ts
+++ src/OpenAI/Services/Example.ts
@@ -1,34 +1,53 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { openaiserver } from './Const';
-import { ExampleModel, DefaultExampleModel } from '../Models/ExampleModel';
+import axios, { AxiosRequestConfig } from 'axios'
+import { openaiserver } from './Const'
+import { ExampleModel, DefaultExampleModel } from '../Models/ExampleModel'
 
 const settingPath = 'examples/'
 
 const getExample = async (exampleName: string) => {
     const examples = await getAllExamples()
     const example = examples.find(d => d.key == exampleName) ?? DefaultExampleModel
-    return example;
+    return example
 }
 
 const getAllExamples = async () => {
     const options: AxiosRequestConfig = {
         url: `${openaiserver}${settingPath}`,
         method: "GET",
-    };
-
-    let result: ExampleModel[] = [];
-    try {
-        const { data, status } = await axios<ExampleModel[]>(options);
-        result = data
-    } catch (error) {
-        console.log(error);
     }
 
-    return result;
+    let result: ExampleModel[] = []
+    try {
+        const { data, status } = await axios<ExampleModel[]>(options)
+        result = data
+    } catch (error) {
+        console.log(error)
+    }
+
+    return result
 }
 
+const createExample = async (data: ExampleModel) => {
+    const options: AxiosRequestConfig = {
+        url: `${openaiserver}${settingPath}`,
+        method: "POST",
+        data: data,
+    }
+
+    let result: ExampleModel[] = []
+
+    try {
+        const { data, status } = await axios<ExampleModel[]>(options)
+        return data
+    } catch (error: any) {
+        console.log(error)
+    }
+
+    return result
+}
 
 export default {
     getExample: getExample,
     getAllExamples: getAllExamples,
+    createExample: createExample,
 }
