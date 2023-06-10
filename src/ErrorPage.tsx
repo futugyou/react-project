89f3a3b4530b204843671fb40a09efa4c0ebd11c
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-10 16:49:20 +0800

    prompt message when the user does not login

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
 
-function ErrorPage() {
+function ErrorPage(props: any) {
+    const message = props.message || "Nothing to see here!";
+    
     return (
         <div>
-            <h2>Nothing to see here!</h2>
+            <h2>{message}</h2>
             <p>
                 <Link to="/">Go to the home page</Link>
             </p>
--- src/GuardedRoute.tsx
+++ src/GuardedRoute.tsx
@@ -1,6 +1,5 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
 import { useAuth } from './Auth/index'
+import ErrorPage from './ErrorPage'
 
 interface GuardedRouteProps {
     children?: React.ReactNode;
@@ -11,7 +10,10 @@ function GuardedRoute({ children }: GuardedRouteProps) {
     const { authService } = useAuth()
     const isRouteAccessible = authService.isAuthenticated()
     if (!isRouteAccessible) {
-        return (<></>)
+        return (
+            <ErrorPage message="This page is only accessible to logged-in users">
+            </ErrorPage>
+        )
     } else {
         return (<>{children}</>)
     }
