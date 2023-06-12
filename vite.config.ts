cb447109dddfb4197922efc9ecf6f23c32090051
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-12 13:42:12 +0800

    update cros for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   envPrefix: "REACT_APP_",
+  server: {
+    cors: {
+      origin: true,
+      methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
+      allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "Authorization", "Access-Control-Allow-Headers", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "x-requested-with"],
+      exposedHeaders: ["Access-Control-Allow-Origin", "Origin", "Authorization", "Access-Control-Allow-Headers", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "x-requested-with"],
+      credentials: true
+    }
+  }
   // define: {
   //   global: {}
   // }
