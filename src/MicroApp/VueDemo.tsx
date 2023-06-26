b8d9502d14de09541775f37c832a16a890e2c2c1
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-26 16:22:14 +0800

    mirco-app, redirect to sub app when login

--- src/MicroApp/VueDemo.tsx
+++ src/MicroApp/VueDemo.tsx
@@ -2,12 +2,17 @@
 /** @jsx jsxCustomEvent */
 import jsxCustomEvent from '@micro-zoe/micro-app/polyfill/jsx-custom-event'
 import { useState } from 'react'
+import { useAuth } from '../Auth/index'
+import microApp from '@micro-zoe/micro-app'
 
 const VueDemo = () => {
+    const { authService } = useAuth()
+
     const jwtToken = JSON.parse(window.localStorage.getItem('auth') || '{}')
     const [microAppData, changeMicroAppData] = useState({
         msg: 'data from base app',
         "Authorization": "Bearer " + jwtToken.access_token,
+        crateAt: Date(),
     })
     const vueawsapp_address = import.meta.env.REACT_APP_VUEAWS_APP_ADDRESS
 
@@ -23,10 +28,21 @@ const VueDemo = () => {
         console.log(5, 'vueawsapp rendered')
 
         setTimeout(() => {
-            changeMicroAppData({
-                msg: 'new data from base app',
-                "Authorization": "Bearer " + jwtToken.access_token,
-            })
+            const token = JSON.parse(window.localStorage.getItem('auth') || '{}')
+            if (token.access_token) {
+                changeMicroAppData({
+                    msg: 'new data from base app',
+                    Authorization: "Bearer " + token.access_token,
+                    crateAt: Date(),
+                })
+            } else {
+                changeMicroAppData({
+                    msg: 'new data from base app',
+                    Authorization: '',
+                    crateAt: Date(),
+                })
+            }
+
         }, 2000)
     }
 
@@ -40,6 +56,13 @@ const VueDemo = () => {
 
     const handleDataChange = (e: CustomEvent) => {
         console.log(7, 'data from vue demo app:', e.detail.data)
+        if (e.detail.data.loginRedirect) {
+            const isRouteAccessible = authService.isAuthenticated()
+            if (!isRouteAccessible) {
+                authService.authorize()
+            }
+            microApp.router.push({ name: 'vueawsapp', path: e.detail.data.loginRedirect })
+        }
     }
 
     return (
