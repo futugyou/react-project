297ce15a2fc151bfabafe1df69bd9566b7fc77bd
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-18 20:27:56 +0800

    update auth service config

--- src/Auth/Auth.tsx
+++ src/Auth/Auth.tsx
@@ -1,12 +1,12 @@
 import { AuthService } from './AuthService'
 
 export const authService = new AuthService({
-    clientId: import.meta.env.REACT_APP_CLIENT_ID,
-    location: window.location,
-    provider: import.meta.env.REACT_APP_PROVIDER,    
-    authorizeEndpoint: import.meta.env.REACT_APP_AUTHORIZE,
-    tokenEndpoint: import.meta.env.REACT_APP_TOKEN,
-    redirectUri: import.meta.env.REACT_APP_REDIRECT_URI || window.location.origin,
-    scopes: ['openid', 'profile']
-  });
- 
+  clientId: import.meta.env.REACT_APP_CLIENT_ID,
+  location: window.location,
+  provider: import.meta.env.REACT_APP_PROVIDER,
+  authorizeEndpoint: import.meta.env.REACT_APP_AUTHORIZE,
+  tokenEndpoint: import.meta.env.REACT_APP_TOKEN,
+  redirectUri: import.meta.env.REACT_APP_REDIRECT_URI || window.location.origin,
+  autoRefresh: true,
+  scopes: ['openid', 'profile']
+});
