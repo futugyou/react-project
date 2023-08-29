07b2676d0d2c8091db099c02f056f5c47a9fb710
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-29 15:53:55 +0800

    add classnode flow component

---
+++ src/Flow/ClassNode.tsx
@@ -0,0 +1,63 @@
+import './ClassNode.css'
+
+import { ReactElement, JSXElementConstructor, ReactNode } from 'react'
+import { Handle, Position, Node, NodeProps } from 'reactflow'
+
+export type ClassNodeData = {
+    name: string
+    parent?: string
+    methods?: string[]
+    propertys?: string[]
+}
+
+export type ClassNodeType = Node<ClassNodeData>
+
+export const ClassNode = ({ data }: NodeProps<ClassNodeData>) => {
+    let methods: JSX.Element[] = []
+    if (data.methods) {
+        methods = data.methods.map((t: string) => {
+            return (
+                <li key={t} >
+                    {t}
+                </li>
+            )
+        })
+    }
+
+    let propertys: JSX.Element[] = []
+    if (data.propertys) {
+        propertys = data.propertys.map((t: string) => {
+            return (
+                <li key={t} >
+                    {t}
+                </li>
+            )
+        })
+    }
+
+    return (
+        <div className='node-container'>
+            <div className='class-name' >
+                {data.name} {data.parent ? " : " + data.parent : ""}
+            </div>
+
+            {propertys.length > 0 && (
+                <div>
+                    <ul className='list-display'>
+                        {propertys}
+                    </ul>
+                </div>
+            )}
+
+            {methods.length > 0 && (
+                <div>
+                    <ul className='list-display'>
+                        {methods}
+                    </ul>
+                </div>
+            )}
+
+            <Handle type="source" position={Position.Bottom} id="a" />
+        </div>
+    )
+}
--- src/Flow/Dotnet/DI.tsx
+++ src/Flow/Dotnet/DI.tsx
@@ -1,15 +1,61 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import ReactFlow, {
-    MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge, BackgroundVariant,Panel
+    NodeTypes, Edge, Node, MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge, BackgroundVariant, Panel
 } from 'reactflow'
 
 import 'reactflow/dist/style.css'
+import { ClassNode } from '../ClassNode'
 
-const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+const initialNodes: Node[] = [
+    {
+        id: 'ServiceDescriptor', type: 'custom', position: { x: 0, y: 0 },
+        data: {
+            name: 'ServiceDescriptor',
+            propertys: [
+                'public ServiceLifetime Lifetime',
+                'public Type ServiceType',
+                'public Type? ImplementationType',
+                'public Type? KeyedImplementationType',
+                'public object? ImplementationInstance',
+                'public Func<IServiceProvider, object>? ImplementationFactory'
+
+            ],
+            methods: [
+                'public static ServiceDescriptor Transient<TService, TImplementation>()',
+                'public static ServiceDescriptor KeyedTransient<TService, TImplementation>(object? serviceKey)',
+                'public static ServiceDescriptor Scoped<TService, TImplementation>()',
+                'public static ServiceDescriptor Singleton<TService, TImplementation>()',
+                'public static ServiceDescriptor Describe(Type serviceType, Type implementationType, ServiceLifetime lifetime)',
+            ]
+        }
+    },
+    {
+        id: 'IServiceCollection ', position: { x: 0, y: 300 }, type: 'custom',
+        data: {
+            name: 'IServiceCollection',
+            parent: 'IList<ServiceDescriptor>',
+            propertys: [
+                'public int Count',
+                'public ServiceDescriptor this[int index]',
+
+            ],
+            methods: [
+                'void Add(ServiceDescriptor item)',
+                'public static IServiceCollection AddTransient(this IServiceCollection services, Type serviceType, Type implementationType)',
+                'public static IServiceCollection AddKeyedTransient(this IServiceCollection services, Type serviceType, object? serviceKey, Type implementationType)',
+                'public static IServiceCollection AddScoped(this IServiceCollection services, Type serviceType, Func<IServiceProvider, object> implementationFactory)',
+                'public static IServiceCollection AddSingleton<TService>(this IServiceCollection services, TService implementationInstance)',
+                'public static ServiceProvider BuildServiceProvider(this IServiceCollection services)',                
+            ]
+        }
+    },
 ]
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
+
+const initialEdges: Edge[] = [{ id: 'e1-2', source: 'ServiceDescriptor', target: 'IServiceCollection' }]
+
+const nodeTypes: NodeTypes = {
+    custom: ClassNode,
+}
 
 export default function App() {
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
@@ -28,6 +74,7 @@ export default function App() {
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
                 fitView
+                nodeTypes={nodeTypes}
             >
                 <Panel position="top-left">
                     <h2>Dependency Injection</h2>
