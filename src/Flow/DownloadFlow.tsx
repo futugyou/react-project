4be72ae10aeeeea9cbd7f3c61209bef537ee0c93
futugyou <17871902+futugyou@users.noreply.github.com>
2023-09-04 19:51:54 +0800

    remove image options

--- src/Flow/DownloadFlow.tsx
+++ src/Flow/DownloadFlow.tsx
@@ -24,14 +24,14 @@ function DownloadFlow() {
         const flow = document.querySelector('.react-flow__viewport') as HTMLElement
         if (flow) {
             toPng(flow, {
-                backgroundColor: '#1a365d',
-                width: imageWidth,
-                height: imageHeight,
-                style: {
-                    width: imageWidth + '',
-                    height: imageHeight + '',
-                    transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
-                },
+                // backgroundColor: '#1a365d',
+                // width: imageWidth,
+                // height: imageHeight,
+                // style: {
+                //     width: imageWidth + '',
+                //     height: imageHeight + '',
+                //     transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
+                // },
             }).then(downloadImage)
         }
 
