0d8563386ff5bfe2d0d9957bdbeac1a790c8b534
futugyou <17871902+futugyou@users.noreply.github.com>
2023-09-11 19:08:30 +0800

    add menu folding, use fake data

--- src/Flow/FlowPanel.tsx
+++ src/Flow/FlowPanel.tsx
@@ -1,10 +1,12 @@
 import './FlowPanel.css'
 
-import React from "react"
+import React, { useState } from "react"
 import { Outlet, NavLink } from "react-router-dom"
+
 import { FlowRouteDataList } from './FlowRouteData'
 
 const FlowPanel = (props: any) => {
+    const [showIndex, setShowIndex] = useState(0)
     const links = FlowRouteDataList.map(a => {
         return (
             <li className="nav-item" key={a.name}>
@@ -12,18 +14,36 @@ const FlowPanel = (props: any) => {
             </li>
         )
     })
+
+    const onGroupClick = (index: number) => {
+        setShowIndex(index)
+    }
+
     return (
         <>
             <div className="flow-panel-container">
                 <div className="flow-panel-menu" >
-                    <ul className="nav nav-pills flex-column mb-auto">
+                    <div className='flow-panel-menu-group'>
+                        <div className='menu-group-name' onClick={() => onGroupClick(0)}>
+                            NetCore
+                        </div>
+                        {showIndex == 0 && (<ul className="nav nav-pills flex-column mb-auto">
+                            {links}
+                        </ul>)}
+                    </div>
+                    <div className='flow-panel-menu-group'>
+                        <div className='menu-group-name' onClick={() => onGroupClick(1)} >
+                            Fake
+                        </div>
+                    {showIndex == 1 && (<ul className="nav nav-pills flex-column mb-auto">
                         {links}
-                    </ul>
-                </div>
-                <div className="flow-panel-content">
-                    <Outlet />
+                    </ul>)}
                 </div>
             </div>
+            <div className="flow-panel-content">
+                <Outlet />
+            </div>
+        </div>
         </>
     )
 }
