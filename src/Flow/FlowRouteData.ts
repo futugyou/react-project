c6723464177765269c8d03a83044fdb19b740d4e
futugyou <17871902+futugyou@users.noreply.github.com>
2023-09-18 12:40:48 +0800

    add empty session flow

--- src/Flow/FlowRouteData.ts
+++ src/Flow/FlowRouteData.ts
@@ -135,11 +135,18 @@ export const FlowRouteDataList: FlowRouteData[] = [{
     linkpath: '/flow/dotnet/exception',
     path: 'dotnet/exception',
     element: lazy(() => import('./Dotnet/Exception/Flow')),
-},{
+}, {
     name: 'httpcache',
     display: 'HttpCache',
     group: 'NetCore',
     linkpath: '/flow/dotnet/httpcache',
     path: 'dotnet/httpcache',
     element: lazy(() => import('./Dotnet/HttpCache/Flow')),
+}, {
+    name: 'session',
+    display: 'Session',
+    group: 'NetCore',
+    linkpath: '/flow/dotnet/session',
+    path: 'dotnet/session',
+    element: lazy(() => import('./Dotnet/Session/Flow')),
 },]
