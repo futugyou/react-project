bf5942ed52c9bbd1a108ba38ef0f000bc62f95d0
futugyou <17871902+futugyou@users.noreply.github.com>
2023-09-24 17:09:08 +0800

    add create node flow

---
+++ src/Flow/MiscFeatures/CreateNode.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { ClassNodeData, ClassNodeType, DefaultClassNodeType } from '@/Flow/CustomNode/ClassNode'
+
+import MiniModal from '@/Common/MiniModal'
+
+import { ModifyNode } from '@/Flow/CustomNode/ModifyNode'
+
+interface CreateNodeProps {
+    addOrUpdtateNode: ClassNodeType
+    setAddOrUpdtateNode: (arg0: ClassNodeType) => void
+    updateNode: (data: ClassNodeData) => void
+}
+
+const CreateNode = (props: CreateNodeProps) => {
+    const [showModal, setShowModal] = useState(false)
+    const onNodeAdd = () => {
+        const newNode = DefaultClassNodeType
+        props.setAddOrUpdtateNode(newNode)
+        setShowModal(true)
+    }
+
+    const updateNode = (data: ClassNodeData) => {
+        props.updateNode(data)
+        setShowModal(false)
+    }
+
+    return (
+        <>
+            <MiniModal show={showModal} setShow={setShowModal}  >
+                <ModifyNode data={props.addOrUpdtateNode.data} updateNode={updateNode} ></ModifyNode>
+            </MiniModal>
+            <button onClick={onNodeAdd}>addNode</button>
+        </>
+
+    )
+}
+
+export default CreateNode
