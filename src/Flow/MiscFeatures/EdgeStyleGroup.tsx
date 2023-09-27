ada9d77e86bf19dc2153eeb13088630add5562d2
futugyou <17871902+futugyou@users.noreply.github.com>
2023-09-27 20:20:40 +0800

    update edge type svg

--- src/Flow/MiscFeatures/EdgeStyleGroup.tsx
+++ src/Flow/MiscFeatures/EdgeStyleGroup.tsx
@@ -19,25 +19,29 @@ const EdgeStyleGroup = (props: EdgeStyleGroupProps) => {
                     <div className={styles.groupLayer}>
                         <div className={styles.groupLayerItem}>
                             <svg width='40' height='40' xmlns='http://wwww.w3.org/2000/svg'>
-                                <title>bezier</title>
-                                <path d="M 0 0 C 0 0, 0 40, 40 40" stroke="black" fill="transparent" />
+                                <title>Bezier</title>
+                                <path d="M 0 0 Q 0 40, 40 40" stroke="black" fill="transparent" />
+                            </svg>
+                        </div>
+                        <div className={styles.groupLayerItem}>
+                            <svg width="40" height="40" xmlns="http://wwww.w3.org/2000/svg">
+                                <title>SmoothStep</title>
+                                <path d="M 5 0 V 10" stroke="black" fill="transparent"></path>
+                                <path d="M 5 10 Q 5 20, 15 20," stroke="black" fill="transparent"></path>
+                                <path d="M 15 20 H 25" stroke="black" fill="transparent"></path>
+                                <path d="M 25 20 Q 35 20, 35 30," stroke="black" fill="transparent"></path>
+                                <path d="M 35 30 V 40" stroke="black" fill="transparent"></path>
                             </svg>
                         </div>
                         <div className={styles.groupLayerItem}>
                             <svg width='40' height='40' xmlns='http://wwww.w3.org/2000/svg'>
-                                <title>smoothStep</title>
-
-                            </svg>
-                        </div>
-                        <div className={styles.groupLayerItem}>
-                            <svg width='40' height='40' xmlns='http://wwww.w3.org/2000/svg'>
-                                <title>straight</title>
+                                <title>Straight</title>
                                 <path d="M 0 0 L 40 40" stroke="black" fill="transparent" />
                             </svg>
                         </div>
                         <div className={styles.groupLayerItem}>
                             <svg width='40' height='40' xmlns='http://wwww.w3.org/2000/svg'>
-                                <title>step</title>
+                                <title>Step</title>
                                 <path d="M 10 0 V 20" stroke="black" fill="transparent" />
                                 <path d="M 10 20 H 30" stroke="black" fill="transparent" />
                                 <path d="M 30 20 V 40" stroke="black" fill="transparent" />
