3cde41bec92c7a2e9c644364e08bcf4368df86a9
futugyou <17871902+futugyou@users.noreply.github.com>
2023-10-08 17:10:29 +0800

    update shape node

--- src/Flow/CustomNode/ShapeNode.tsx
+++ src/Flow/CustomNode/ShapeNode.tsx
@@ -1,8 +1,12 @@
 import styles from './ShapeNode.module.css'
 
 import { useState, useEffect } from 'react'
-import { Handle, useStore, ReactFlowState, Position, NodeProps, useNodeId, useReactFlow, NodeResizer, ResizeDragEvent, ResizeParams } from 'reactflow'
+import {
+    useStore, ReactFlowState, NodeProps, useNodeId, useReactFlow, NodeResizer,
+    ResizeDragEvent, ResizeParams
+} from 'reactflow'
 import { NodeOperation } from './utils'
+import HandlePlus from './HandlePlus'
 
 interface ShapeNodeData {
     shape?: 'diamond' | 'circle' | 'ellipse' | 'parallelogram' | 'rect' | 'radiusrect' | 'db' | 'bus'
@@ -66,6 +70,7 @@ const ShapeNode = (props: NodeProps<ShapeNodeData>) => {
     const connectionNodeId = useStore(connectionNodeIdSelector)
     const isConnecting = !!connectionNodeId
     const isTarget = connectionNodeId && connectionNodeId !== props.id
+
     const nodeId = useNodeId()!
     const { getNode, setNodes } = useReactFlow()
     const node = getNode(nodeId)!
@@ -112,15 +117,8 @@ const ShapeNode = (props: NodeProps<ShapeNodeData>) => {
     return (
         <div className={styles.ShapeNode}>
             {(props.data?.op?.allowResizer ?? true) && (<NodeResizer minWidth={30} minHeight={30} onResize={onResize} onResizeEnd={onResizeEnd} isVisible={props.selected} keepAspectRatio={keepAspectRatio} lineClassName={styles.ShapeNodeResizerLine} handleClassName={styles.ShapeNodeResizerHandle} />)}
-            <Handle id={props.id + '01'} key={props.id + '01'} position={Position.Top} type='source' className={`${props.selected ? styles.nodeHandleDisplay : styles.nodeHandleHidden}`} />
-            <Handle id={props.id + '02'} key={props.id + '02'} position={Position.Bottom} type='source' className={`${props.selected ? styles.nodeHandleDisplay : styles.nodeHandleHidden}`} />
-            <Handle id={props.id + '03'} key={props.id + '03'} position={Position.Left} type='source' className={`${props.selected ? styles.nodeHandleDisplay : styles.nodeHandleHidden}`} />
-            <Handle id={props.id + '04'} key={props.id + '04'} position={Position.Right} type='source' className={`${props.selected ? styles.nodeHandleDisplay : styles.nodeHandleHidden}`} />
-            {/* <Handle id={props.id + '05'} key={props.id + '05'} style={{ top: height / 2, left: width / 2 }} position={Position.Top} type='target' className={styles.nodeHandleHidden2} /> */}
-            <Handle id={props.id + '05'} key={props.id + '05'} position={Position.Top} type='target' className={styles.nodeHandleHidden2} />
-            <Handle id={props.id + '06'} key={props.id + '06'} position={Position.Bottom} type='target' className={styles.nodeHandleHidden2} />
-            <Handle id={props.id + '07'} key={props.id + '07'} position={Position.Left} type='target' className={styles.nodeHandleHidden2} />
-            <Handle id={props.id + '08'} key={props.id + '08'} position={Position.Right} type='target' className={styles.nodeHandleHidden2} />
+
+            <HandlePlus id={props.id} selected={props.selected}></HandlePlus>
             <div className={styles.nodeDisplayContainer}>
                 <div className={styles.nodeDisplayLable} style={{ color: node.style?.color }}>{props.data.label}</div>
             </div>
