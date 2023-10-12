5ceb95ffe752b96fbf48a64dd7f91157576af65f
futugyou <17871902+futugyou@users.noreply.github.com>
2023-10-12 16:09:07 +0800

    add cy event

--- src/CytoscapeGraph/CytoscapePanel.tsx
+++ src/CytoscapeGraph/CytoscapePanel.tsx
@@ -3,8 +3,9 @@ import '@cloudscape-design/global-styles/index.css'
 
 import React, { useCallback, useEffect, useState } from 'react'
 import Select from "@cloudscape-design/components/select"
+import Button from "@cloudscape-design/components/button"
 
-import Cytoscape from 'cytoscape'
+import cytoscape from 'cytoscape'
 import avsdf from 'cytoscape-avsdf'
 import euler from 'cytoscape-euler'
 import fcose from 'cytoscape-fcose'
@@ -15,9 +16,9 @@ import { graphStyle } from './Styling/GraphStyling'
 
 import data from './data/data.json'
 
-Cytoscape.use(avsdf)
-Cytoscape.use(euler)
-Cytoscape.use(fcose)
+cytoscape.use(avsdf)
+cytoscape.use(euler)
+cytoscape.use(fcose)
 
 const CytoscapePanel = () => {
     const [selectedOption, setSelectedOption] = React.useState({ label: "fcose", value: "fcose" })
@@ -48,28 +49,105 @@ const CytoscapePanel = () => {
 
     const layout = { name: selectedOption.value }
 
-    const cyCallback = useCallback((cy: Cytoscape.Core) => {
-        cy.on('resize', () => cy.fit(undefined, 20))
+    const handleDoubleTap = useCallback((event: cytoscape.EventObject, extraParams?: any) => {
+        const node = event.target
+        let position = node.position()
+        console.log('handleDoubleTap', node)
+        const nodeid = `random_nodeid_${+new Date()}`
+        const edgeid = `random_edgeid_${+new Date()}`
+        event.cy.add({
+            group: 'nodes',
+            data: { id: nodeid, weight: 75 },
+            position: { x: position.x + 30, y: position.y + 30 },
+            "selected": false,
+            "selectable": true,
+            "locked": false,
+            "grabbable": true,
+            "classes": ""
+        })
+
+        event.cy.add({
+            "data": {
+                "id": edgeid,
+                "sbgnclass": "consumption",
+                "sbgncardinality": 0,
+                "source": nodeid,
+                "target": node.id(),
+                "portsource": nodeid,
+                "porttarget": node.id()
+            },
+            "group": "edges",
+            "selected": false,
+            "selectable": true,
+            "locked": false,
+            "grabbable": true,
+            "classes": ""
+        })
+
+        node.layout(layout).run()
     }, [])
+
+    const handleTap = useCallback((event: cytoscape.EventObject, extraParams?: any) => {
+        const node = event.target
+        console.log('handleTap', node)
+    }, [])
+
+    const cyCallback = useCallback((cy: cytoscape.Core) => {
+        cy.removeListener('select', 'node')
+        cy.removeListener('dbltap', 'node')
+        cy.on('dbltap', 'node', handleDoubleTap)
+        cy.on('select', 'node', handleTap)
+        cy.on('resize', () => cy.fit(undefined, 120))
+        cy.minZoom(0.25)
+        cy.maxZoom(2.0)
+        cy.on('tapdragover', 'node', function (evt) {
+            const node = evt.target
+            node.unlock()
+            node.grabify()
+            node.descendants().unlock()
+            node.descendants().grabify()
+        })
+        cy.on('tapdragout', 'node', function (evt) {
+            const node = evt.target
+            node.lock()
+            node.ungrabify()
+            node.descendants().lock()
+            node.descendants().ungrabify()
+        })
+    }, [handleDoubleTap, handleTap])
+
     return (
         <div className="cytoscapePanel">
-            <div className='layoutSelector'>
-                <Select
-                    selectedOption={selectedOption}
-                    onChange={onSelectChange}
-                    options={[
-                        { label: "avsdf", value: "avsdf" },
-                        { label: "euler", value: "euler" },
-                        { label: "fcose", value: "fcose" },
-                        { label: "random", value: "random" },
-                        { label: "grid", value: "grid" },
-                        { label: "circle", value: "circle" },
-                        { label: "concentric", value: "concentric" },
-                        { label: "breadthfirst", value: "breadthfirst" },
-                        { label: "cose", value: "cose" },
-                    ]}
-                />
+            <div className='layoutController'>
+                <div className='controllerItem'>
+                    <div className="itemDescription">change layout</div>
+                    <div className="itemContent">
+                        <Select
+                            selectedOption={selectedOption}
+                            onChange={onSelectChange}
+                            options={[
+                                { label: "avsdf", value: "avsdf" },
+                                { label: "euler", value: "euler" },
+                                { label: "fcose", value: "fcose" },
+                                { label: "random", value: "random" },
+                                { label: "grid", value: "grid" },
+                                { label: "circle", value: "circle" },
+                                { label: "concentric", value: "concentric" },
+                                { label: "cose", value: "cose" },
+                            ]}
+                        />
+                    </div>
+                </div>
+                <div className='controllerItem'>
+                    <div className="itemDescription">download image</div>
+                    <div className="itemContent">
+                        <Button>
+                            Download
+                        </Button>
+                    </div>
+                </div>
             </div>
+
             <CytoscapeComponent
                 cy={cyCallback}
                 elements={elements3}
