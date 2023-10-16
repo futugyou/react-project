a10e8753a3b11adf6c4adcbbabc0f6e34497ba3b
futugyou <17871902+futugyou@users.noreply.github.com>
2023-10-16 14:42:05 +0800

    add aws config resource hook

---
+++ src/CytoscapeGraph/useGetResourceGraph.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query'
+import axios, { AxiosRequestConfig } from 'axios'
+
+const configserver = import.meta.env.REACT_APP_FLOW_SERVER
+
+const configPath = 'v1/awsconfig'
+
+export function useGetResourceGraph() {
+    const jwtToken = JSON.parse(window.localStorage.getItem('auth') || '{}')
+    const options: AxiosRequestConfig = {
+        url: configserver + configPath,
+        method: "GET",
+        headers: {
+            'Account-Id': 'aws-account-id-magic-code'
+        },
+    }
+
+    options.headers!.Authorization = "Bearer " + jwtToken.access_token
+
+    const { isLoading, isError, data, refetch, isFetching } = useQuery({
+        queryKey: [configPath],
+        queryFn: () => axios(options).then(x => x.data),
+    })
+
+    return {
+        data,
+        isLoading,
+        isFetching,
+        isError,
+        refetch,
+        status: getStatus(isFetching, isError)
+    }
+}
+
+export const getStatus = (isFetching: boolean, isError: boolean) => {
+    if (isFetching) {
+        return 'loading'
+    }
+    return isError ? 'error' : 'finished'
+}
