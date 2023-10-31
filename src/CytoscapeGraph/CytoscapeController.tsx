d5594f4acefa80986f346ced48064c7f2b273247
futugyou <17871902+futugyou@users.noreply.github.com>
2023-10-31 20:14:29 +0800

    update icon

--- src/CytoscapeGraph/CytoscapeController.tsx
+++ src/CytoscapeGraph/CytoscapeController.tsx
@@ -9,7 +9,7 @@ import fcose from 'cytoscape-fcose'
 
 import Select from "@cloudscape-design/components/select"
 import Button from "@cloudscape-design/components/button"
-import { BsListUl } from "react-icons/bs"
+import { BsListUl, BsChevronCompactLeft } from "react-icons/bs"
 
 import { useAuth } from '@/Auth/index'
 
@@ -145,7 +145,7 @@ const CytoscapeController = ({ visible, setVisible }: CytoscapeControllerProps) 
             <div className='controllerHead'>
                 <div className="itemDescription">Cytoscape Controller</div>
                 <div className="itemContent">
-                    <BsListUl className="itemIcon" onClick={() => setVisible(false)} />
+                    <BsChevronCompactLeft className="itemIcon" onClick={() => setVisible(false)} />
                 </div>
             </div>
 
