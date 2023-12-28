accecd5cf6da438afaf74f87475faa11a434b6b3
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-28 16:14:16 +0800

    test fluid model

--- src/Whiteboards/App.tsx
+++ src/Whiteboards/App.tsx
@@ -3,107 +3,118 @@ import { TinyliciousClient } from "@fluidframework/tinylicious-client"
 import { SharedCell } from "@fluidframework/cell"
 import { SignalManager } from "@fluid-experimental/data-objects"
 import { SharedMap, LoadableObjectRecord, ContainerSchema, IMember } from "fluid-framework"
-import { getFluidData, containerSchema } from "./model"
+import { containerSchema } from "./model"
+import { FluidProvider } from "./contexts/FluidProvider"
+import { useModel } from "./hooks/useModel"
 
 const App = () => {
-    const [fluidSharedObjects, setFluidSharedObjects] = useState<LoadableObjectRecord>()
-    const [localTimestamp, setLocalTimestamp] = useState<any>()
-    const [localDynamicMap, setDynamicMap] = useState<any>()
-    const [members, setMembers] = useState<any>()
-    const [myself, setMyself] = useState<any>()
+    const model = useModel()
+    // const [fluidSharedObjects, setFluidSharedObjects] = useState<LoadableObjectRecord>()
+    // const [localTimestamp, setLocalTimestamp] = useState<any>()
+    // const [localDynamicMap, setDynamicMap] = useState<any>()
+    // const [members, setMembers] = useState<any>()
+    // const [myself, setMyself] = useState<any>()
 
-    useEffect(() => {
-        getFluidData()
-            .then(data => {
-                setFluidSharedObjects(data.initialObjects)
-                const ils: any[] = []
-                data.members?.forEach((v: any, k: any) => {
-                    ils.push(<li key={k}>{v.userId} : {v.userName}</li>)
-                });
-                setMembers(ils)
-                setMyself(data.getMyself)
-            })
-    }, [])
-
-
-
-    useEffect(() => {
-        if (fluidSharedObjects) {
-            // 4: Set the value of the localTimestamp state object that will appear in the UI.
-            const { sharedTimestamp, map } = fluidSharedObjects
-            const sharedMap = sharedTimestamp as SharedMap
-            const dynamicMap = map as SharedMap
-            const updateLocalTimestamp = () => setLocalTimestamp({
-                time: sharedMap.get("time"),
-            })
-            updateLocalTimestamp()
-
-            // 5: Register handlers.
-            sharedMap.on("valueChanged", updateLocalTimestamp)
-            const handle = dynamicMap.get("cell-id")
-            handle.get().then((cell: SharedCell) => {
-                cell.on("valueChanged", (d) => {
-                    console.log("valueChanged on dynamic cell: ", d)
-                })
-            })
-            dynamicMap.on("valueChanged", (changed) => {
-                if (changed.key === "cell-id") {
-                    const handle = dynamicMap.get(changed.key);
-                    handle.get().then((cell: SharedCell) => {
-                        console.log("valueChanged on map: ", cell.get())
-                    })
-                }
-            })
-            // 6: Delete handler registration when the React App component is dismounted.
-            return () => {
-                sharedMap.off("valueChanged", updateLocalTimestamp)
-            }
-        } else {
-            return // Do nothing because there is no Fluid SharedMap object yet.
-        }
-    }, [fluidSharedObjects])
-
-    const updateTime = async () => {
-        if (fluidSharedObjects) {
-            const { sharedTimestamp, map } = fluidSharedObjects
-            const sharedMap = sharedTimestamp as SharedMap
-            sharedMap.set("time", Date.now().toString())
+    // useEffect(() => {
+    //     getFluidData()
+    //         .then(data => {
+    //             setFluidSharedObjects(data.initialObjects)
+    //             const ils: any[] = []
+    //             data.members?.forEach((v: any, k: any) => {
+    //                 ils.push(<li key={k}>{v.userId} : {v.userName}</li>)
+    //             });
+    //             setMembers(ils)
+    //             setMyself(data.getMyself)
+    //         })
+    // }, [])
 
 
-            const dynamicMap = map as SharedMap
-            const cell = await dynamicMap.get("cell-id").get() as SharedCell
-            cell.set(Date.now().toString())// this will not trigger valueChanged
 
-            const containerId = location.hash.substring(1)
-            const client = new TinyliciousClient()
-            const { container } = await client.getContainer(containerId, containerSchema)
-            const newCell = await container.create(SharedCell); // Create a new SharedCell
-            newCell.set("new dynamic test data")
-            dynamicMap.set("cell-id", newCell.handle); // Attach the new SharedCell
-        }
+    // useEffect(() => {
+    //     if (fluidSharedObjects) {
+    //         // 4: Set the value of the localTimestamp state object that will appear in the UI.
+    //         const { sharedTimestamp, map } = fluidSharedObjects
+    //         const sharedMap = sharedTimestamp as SharedMap
+    //         const dynamicMap = map as SharedMap
+    //         const updateLocalTimestamp = () => setLocalTimestamp({
+    //             time: sharedMap.get("time"),
+    //         })
+    //         updateLocalTimestamp()
+
+    //         // 5: Register handlers.
+    //         sharedMap.on("valueChanged", updateLocalTimestamp)
+    //         const handle = dynamicMap.get("cell-id")
+    //         handle.get().then((cell: SharedCell) => {
+    //             cell.on("valueChanged", (d) => {
+    //                 console.log("valueChanged on dynamic cell: ", d)
+    //             })
+    //         })
+    //         dynamicMap.on("valueChanged", (changed) => {
+    //             if (changed.key === "cell-id") {
+    //                 const handle = dynamicMap.get(changed.key);
+    //                 handle.get().then((cell: SharedCell) => {
+    //                     console.log("valueChanged on map: ", cell.get())
+    //                 })
+    //             }
+    //         })
+    //         // 6: Delete handler registration when the React App component is dismounted.
+    //         return () => {
+    //             sharedMap.off("valueChanged", updateLocalTimestamp)
+    //         }
+    //     } else {
+    //         return // Do nothing because there is no Fluid SharedMap object yet.
+    //     }
+    // }, [fluidSharedObjects])
+
+    // const updateTime = async () => {
+    //     if (fluidSharedObjects) {
+    //         const { sharedTimestamp, map } = fluidSharedObjects
+    //         const sharedMap = sharedTimestamp as SharedMap
+    //         sharedMap.set("time", Date.now().toString())
+
+
+    //         const dynamicMap = map as SharedMap
+    //         const cell = await dynamicMap.get("cell-id").get() as SharedCell
+    //         cell.set(Date.now().toString())// this will not trigger valueChanged
+
+    //         const containerId = location.hash.substring(1)
+    //         const client = new TinyliciousClient()
+    //         const { container } = await client.getContainer(containerId, containerSchema)
+    //         const newCell = await container.create(SharedCell); // Create a new SharedCell
+    //         newCell.set("new dynamic test data")
+    //         dynamicMap.set("cell-id", newCell.handle); // Attach the new SharedCell
+    //     }
+    // }
+
+    const updateTime = () => {
+        model.model?.setSharedTimestamp()
     }
-
-    if (localTimestamp) {
-        return (
-            <div className="App">
-                <button onClick={() => updateTime()}>
-                    Get Time
-                </button>
-                <span>{localTimestamp.time}</span>
-                <div>
-                    <ul>
-                        {members}
-                    </ul>
-                </div>
-                <h1>-</h1>
-                <div>
-                    {myself?.userName}
-                </div>
+    return (
+        <div className="App">
+            <button onClick={() => updateTime()}>
+                Get Time
+            </button>
+            <span>{model.model?.getSharedTimestamp()}</span>
+            <div>
+                <ul>
+                    {/* {members} */}
+                </ul>
             </div>
-        )
-    } else {
-        return <div />
-    }
+            <h1>-</h1>
+            <div>
+                {/* {myself?.userName} */}
+            </div>
+        </div>
+    )
+
 }
 
-export default App
+const AppProvider = () => {
+    return (
+        <FluidProvider>
+            <App></App>
+        </FluidProvider>
+    )
+}
+
+export default AppProvider  
