64024311b107cc0c89ff40e5d1c7d3a45fd3c37f
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-04 02:31:16 +0800

    remove env check in excalidraw route
    remove env check in excalidraw route

--- src/Excalidraw/Route.tsx
+++ src/Excalidraw/Route.tsx
@@ -2,13 +2,7 @@
 import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
 
-let ExcalidrawPage
-const DivEmpty = () => { return <div /> }
-if (process.env.NODE_ENV == 'development') {
-    ExcalidrawPage = lazy(() => import('./App'))
-} else {
-    ExcalidrawPage = DivEmpty
-}
+const ExcalidrawPage = lazy(() => import('./App'))
 
 export const ExcalidrawRoute: RouteObject = {
     path: "/excalidraw",
