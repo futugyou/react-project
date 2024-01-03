8742a30be15abea2516344d1a768fe17c7006fef
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-03 15:04:05 +0800

    Uncaught TypeError: Cannot read property '__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED' of undefined. i can not fix it in production.

--- src/Excalidraw/Route.tsx
+++ src/Excalidraw/Route.tsx
@@ -2,7 +2,13 @@
 import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
 
-const ExcalidrawPage = lazy(() => import('./App'))
+let ExcalidrawPage
+const DivEmpty = () => { return <div /> }
+if (process.env.NODE_ENV == 'development') {
+    ExcalidrawPage = lazy(() => import('./App'))
+} else {
+    ExcalidrawPage = DivEmpty
+}
 
 export const ExcalidrawRoute: RouteObject = {
     path: "/excalidraw",
