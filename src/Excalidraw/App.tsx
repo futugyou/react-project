5ddd000b5279c77ae79c95910976095d2ddcf1ba
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-05 06:09:39 +0800

    add initialData for Excalidraw

--- src/Excalidraw/App.tsx
+++ src/Excalidraw/App.tsx
@@ -1,10 +1,24 @@
+import data from './data.json'
+
 import { Excalidraw } from "@excalidraw/excalidraw"
+import { ExcalidrawInitialDataState } from "@excalidraw/excalidraw/types/types"
+
+const elements = data.elements
+const appState = data.appState
+const scrollToContent = data.scrollToContent
+
+const initialData: ExcalidrawInitialDataState = {
+    elements: elements as any,
+    appState: appState,
+    scrollToContent: scrollToContent,
+}
+
 const App = () => {
     return (
         <>
             <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
             <div style={{ height: "100%", width: "100%" }}>
-                <Excalidraw />
+                <Excalidraw initialData={initialData} />
             </div>
         </>
     )
