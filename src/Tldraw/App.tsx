873b6ba055ce786998adbe9fa599ad845ac93a24
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-08 17:55:00 +0800

    use tldraw api

--- src/Tldraw/App.tsx
+++ src/Tldraw/App.tsx
@@ -1,10 +1,67 @@
-import { Tldraw } from '@tldraw/tldraw'
+import {
+	DefaultColorStyle,
+	Editor,
+	TLGeoShape,
+	TLShapePartial,
+	Tldraw,
+	createShapeId,
+	useEditor,
+} from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 
 export default function () {
+	const handleMount = (editor: Editor) => {
+		// Create a shape id
+		const id = createShapeId('hello')
+
+		// Create a shape
+		editor.createShapes<TLGeoShape>([
+			{
+				id,
+				type: 'geo',
+				x: 128 + Math.random() * 500,
+				y: 128 + Math.random() * 500,
+				props: {
+					geo: 'rectangle' as any,
+					w: 100,
+					h: 100,
+					dash: 'draw' as any,
+					color: 'blue' as any,
+					size: 'm' as any,
+				},
+			},
+		])
+
+		// Get the created shape
+		const shape = editor.getShape<TLGeoShape>(id)!
+
+		const shapeUpdate: TLShapePartial<TLGeoShape> = {
+			id,
+			type: 'geo',
+			props: {
+				h: shape.props.h * 3,
+				text: 'hello world!',
+			},
+		}
+
+		// Update the shape
+		editor.updateShapes([shapeUpdate])
+
+		// Select the shape
+		editor.select(id)
+
+		// Rotate the shape around its center
+		editor.rotateShapesBy([id], Math.PI / 8)
+
+		// Clear the selection
+		editor.selectNone()
+
+		// Zoom the camera to fit both shapes
+		editor.zoomToFit()
+	}
 	return (
 		<div style={{ inset: 0 }}>
-			<Tldraw />
+			<Tldraw onMount={handleMount} />
 		</div>
 	)
 }
