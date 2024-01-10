e232f3255e8f729da71fab1c4e814bb234c39480
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-10 15:33:42 +0800

    persisted tldraw

--- src/Tldraw/App.tsx
+++ src/Tldraw/App.tsx
@@ -1,9 +1,12 @@
 import {
+	createTLStore,
+	defaultShapeUtils,
 	Editor,
 	Tldraw,
+	throttle,
 } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
-import { useCallback, useState } from 'react'
+import { useCallback, useLayoutEffect, useState } from 'react'
 
 import { InsideOfEditorContext } from './Hook/InsideOfEditorContext'
 import { FilterStyleUi } from './Hook/FilterStyleUi'
@@ -23,11 +26,12 @@ const customShapeUtils = [CardShapeUtil, HtmlShapeUtil]
 const customTools = [CardShapeTool]
 
 let initflag = false
+const PERSISTENCE_KEY = 'tldraw_persistence_key'
 const App = () => {
-	const handleMount = useCallback((editor: Editor) => {		
+	const handleMount = useCallback((editor: Editor) => {
 		Meta(editor)
 		editor.registerExternalContentHandler('text', ({ point, sources }) => Html(editor, point, sources))
-		
+
 		if (initflag) {
 			return
 		}
@@ -37,6 +41,61 @@ const App = () => {
 		initflag = true
 	}, [Hello, LocalImages])
 
+	const [store] = useState(() => createTLStore({ shapeUtils: [...defaultShapeUtils, ...customShapeUtils] }))
+	const [loadingState, setLoadingState] = useState<
+		{ status: 'loading' } | { status: 'ready' } | { status: 'error'; error: string }
+	>({
+		status: 'loading',
+	})
+
+	useLayoutEffect(() => {
+		setLoadingState({ status: 'loading' })
+
+		// Get persisted data from local storage
+		const persistedSnapshot = localStorage.getItem(PERSISTENCE_KEY)
+
+		if (persistedSnapshot) {
+			try {
+				const snapshot = JSON.parse(persistedSnapshot)
+				store.loadSnapshot(snapshot)
+				setLoadingState({ status: 'ready' })
+			} catch (error: any) {
+				setLoadingState({ status: 'error', error: error.message }) // Something went wrong
+			}
+		} else {
+			setLoadingState({ status: 'ready' }) // Nothing persisted, continue with the empty store
+		}
+
+		// Each time the store changes, run the (debounced) persist function
+		const cleanupFn = store.listen(
+			throttle(() => {
+				const snapshot = store.getSnapshot()
+				localStorage.setItem(PERSISTENCE_KEY, JSON.stringify(snapshot))
+			}, 500)
+		)
+
+		return () => {
+			cleanupFn()
+		}
+	}, [store])
+
+	if (loadingState.status === 'loading') {
+		return (
+			<div className="tldraw__editor">
+				<h2>Loading...</h2>
+			</div>
+		)
+	}
+
+	if (loadingState.status === 'error') {
+		return (
+			<div className="tldraw__editor">
+				<h2>Error!</h2>
+				<p>{loadingState.error}</p>
+			</div>
+		)
+	}
+
 	return (
 		<div style={{ inset: 0 }}>
 			<Tldraw
@@ -45,6 +104,7 @@ const App = () => {
 				tools={customTools}
 				components={components}
 				overrides={uiOverrides}
+				store={store}
 			>
 				{/* <InsideOfEditorContext /> */}
 				<FilterStyleUi />
