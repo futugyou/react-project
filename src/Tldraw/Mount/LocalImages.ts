29635206f60dea6ed5ac5114f6a46c2201bffa12
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-11 13:16:35 +0800

    can not read local file ,wait for github issue

--- src/Tldraw/Mount/LocalImages.ts
+++ src/Tldraw/Mount/LocalImages.ts
@@ -1,4 +1,4 @@
-import { AssetRecordType, Editor,createShapeId } from "@tldraw/tldraw"
+import { AssetRecordType, Editor, createShapeId } from "@tldraw/tldraw"
 
 export const LocalImages = (editor: Editor) => {
 	const id = createShapeId('local-image')
@@ -10,33 +10,33 @@ export const LocalImages = (editor: Editor) => {
 	const assetId = AssetRecordType.createId()
 	const imageWidth = 200
 	const imageHeight = 200
-
-	editor.createAssets([
-		{
-			id: assetId,
-			type: 'image',
-			typeName: 'asset',
-			props: {
-				name: 'tldraw.png',
-				src: './tldraw/tldraw.png', // You could also use a base64 encoded string here
-				w: imageWidth,
-				h: imageHeight,
-				mimeType: 'image/png',
-				isAnimated: true,
-			},
-			meta: {},
-		},
-	])
-	editor.createShape({
-		id:id,
-		type: 'image',
-		// Let's center the image in the editor
-		x: (window.innerWidth - imageWidth) / 2,
-		y: (window.innerHeight - imageHeight) / 2,
-		props: {
-			assetId,
-			w: imageWidth,
-			h: imageHeight,
-		},
-	})
+ 
+	// editor.createAssets([
+	// 	{
+	// 		id: assetId,
+	// 		type: 'image',
+	// 		typeName: 'asset',
+	// 		props: {
+	// 			name: 'tldraw.png',
+	// 			src: '/tldraw/tldraw.png', // You could also use a base64 encoded string here
+	// 			w: imageWidth,
+	// 			h: imageHeight,
+	// 			mimeType: 'image/png',
+	// 			isAnimated: true,
+	// 		},
+	// 		meta: {},
+	// 	},
+	// ])
+	// editor.createShape({
+	// 	id: id,
+	// 	type: 'image',
+	// 	// Let's center the image in the editor
+	// 	x: (window.innerWidth - imageWidth) / 2,
+	// 	y: (window.innerHeight - imageHeight) / 2,
+	// 	props: {
+	// 		assetId,
+	// 		w: imageWidth,
+	// 		h: imageHeight,
+	// 	},
+	// })
 }
