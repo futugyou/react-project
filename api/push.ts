6b480bff92a6b5f64bde7b2b964c27d1435087d3
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-12 13:52:37 +0800

    vercel push serverless api

---
+++ api/push.ts
@@ -0,0 +1,36 @@
+import Pusher from 'pusher'
+
+const {
+    REACT_APP_FLUID_LOCAL_ENDPOINT: appId,
+    REACT_APP_PUSHER_KEY: key,
+    REACT_APP_PUSHER_SECRET: secret,
+    REACT_APP_PUSHER_CLUSTER: cluster,
+} = process.env
+
+const pusher = new Pusher({
+    appId: appId!,
+    key: key!,
+    secret: secret!,
+    cluster: cluster!,
+})
+
+export const config = {
+    runtime: 'nodejs', // this is a pre-requisite
+}
+
+export default async function handler(req, res) {
+    const { x0, x1, y0, y1, color } = req.body
+    try {
+        const response = await pusher.trigger('my-channel', 'my-event',
+            { x0, x1, y0, y1, color },
+        )
+        if (response.ok) {
+            res.status(200).end('sent event succesfully')
+        } else {
+            res.status(response.status).end('sent event succesfully')
+        }
+
+    } catch (e) {
+        res.status(500).end(e.message)
+    }
+}
