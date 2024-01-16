2365fed7ced3a1508264c3e3b921a3c04c00edfe
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-16 13:16:51 +0800

    add plate code plugins

--- src/ReactDemo/PlateEditor.tsx
+++ src/ReactDemo/PlateEditor.tsx
@@ -1,191 +1,53 @@
-import { Plate } from '@udecode/plate-common'
-import { Editor } from '@/plate-components/plate-ui/editor'
 
-export const initialValue = [
+import { initialValue } from './PlateInitValue'
+
+import {
+    createBoldPlugin,
+    createCodePlugin,
+    createItalicPlugin,
+    createStrikethroughPlugin,
+    createUnderlinePlugin,
+} from '@udecode/plate-basic-marks'
+import { createBlockquotePlugin } from '@udecode/plate-block-quote' 
+import { createHeadingPlugin } from '@udecode/plate-heading'
+import { createParagraphPlugin } from '@udecode/plate-paragraph'
+import { createPlugins, Plate } from '@udecode/plate-common'
+import {
+    createCodeBlockPlugin, ELEMENT_CODE_BLOCK,
+    ELEMENT_CODE_LINE, ELEMENT_CODE_SYNTAX
+} from '@udecode/plate-code-block'
+
+import { Editor } from '@/components/plate-ui/editor'
+import { CodeBlockElement } from '@/components/plate-ui/code-block-element'
+import { CodeLineElement } from '@/components/plate-ui/code-line-element'
+import { CodeSyntaxLeaf } from '@/components/plate-ui/code-syntax-leaf'
+
+const plugins = createPlugins(
+    [
+        createParagraphPlugin(),
+        createBlockquotePlugin(),
+        createCodeBlockPlugin(),
+        createHeadingPlugin(),
+
+        createBoldPlugin(),
+        createItalicPlugin(),
+        createUnderlinePlugin(),
+        createStrikethroughPlugin(),
+        createCodePlugin(),
+    ],
     {
-        type: 'h1',
-        children: [
-            {
-                text: '🌳 Blocks',
-            },
-        ],
-        id: '1',
-    },
-    {
-        type: 'p',
-        children: [
-            {
-                text: 'Easily create headings of various levels, from H1 to H6, to structure your content and make it more organized.',
-            },
-        ],
-        id: '2',
-    },
-    {
-        type: 'blockquote',
-        children: [
-            {
-                text: 'Create blockquotes to emphasize important information or highlight quotes from external sources.',
-            },
-        ],
-        id: '3',
-    },
-    {
-        type: 'code_block',
-        lang: 'javascript',
-        children: [
-            {
-                type: 'code_line',
-                children: [
-                    {
-                        text: '// Use code blocks to showcase code snippets',
-                    },
-                ],
-            },
-            {
-                type: 'code_line',
-                children: [
-                    {
-                        text: 'function greet() {',
-                    },
-                ],
-            },
-            {
-                type: 'code_line',
-                children: [
-                    {
-                        text: "  console.info('Hello World!');",
-                    },
-                ],
-            },
-            {
-                type: 'code_line',
-                children: [
-                    {
-                        text: '}',
-                    },
-                ],
-            },
-        ],
-        id: '4',
-    },
-    {
-        type: 'h1',
-        children: [
-            {
-                text: '🌱 Marks',
-            },
-        ],
-        id: '1',
-    },
-    {
-        type: 'p',
-        children: [
-            {
-                text: 'Add style and emphasis to your text using the mark plugins, which offers a variety of formatting options.',
-            },
-        ],
-        id: '2',
-    },
-    {
-        type: 'p',
-        children: [
-            {
-                text: 'Make text ',
-            },
-            {
-                text: 'bold',
-                bold: true,
-            },
-            {
-                text: ', ',
-            },
-            {
-                text: 'italic',
-                italic: true,
-            },
-            {
-                text: ', ',
-            },
-            {
-                text: 'underlined',
-                underline: true,
-            },
-            {
-                text: ', or apply a ',
-            },
-            {
-                text: 'combination',
-                bold: true,
-                italic: true,
-                underline: true,
-            },
-            {
-                text: ' of these styles for a visually striking effect.',
-            },
-        ],
-        id: '3',
-    },
-    {
-        type: 'p',
-        children: [
-            {
-                text: 'Add ',
-            },
-            {
-                text: 'strikethrough',
-                strikethrough: true,
-            },
-            {
-                text: ' to indicate deleted or outdated content.',
-            },
-        ],
-        id: '4',
-    },
-    {
-        type: 'p',
-        children: [
-            {
-                text: 'Write code snippets with inline ',
-            },
-            {
-                text: 'code',
-                code: true,
-            },
-            {
-                text: ' formatting for easy readability.',
-            },
-        ],
-        id: '5',
-    },
-    {
-        type: 'p',
-        children: [
-            {
-                text: 'Press ',
-            },
-            {
-                text: '⌘+B',
-                kbd: true,
-            },
-            {
-                text: ' to apply bold mark or ',
-            },
-            {
-                text: '⌘+I',
-                kbd: true,
-            },
-            {
-                text: ' for italic mark.',
-            },
-        ],
-        id: '6',
-    },
-];
+        components: {
+            [ELEMENT_CODE_BLOCK]: CodeBlockElement,
+            [ELEMENT_CODE_LINE]: CodeLineElement,
+            [ELEMENT_CODE_SYNTAX]: CodeSyntaxLeaf,
+        },
+    }
+)
 
 const BasicEditor = () => {
 
     return (
-        <Plate initialValue={initialValue} >
+        <Plate initialValue={initialValue} plugins={plugins}>
             <Editor spellCheck={false} autoFocus={false} placeholder="Type..." />
         </Plate>
     )
