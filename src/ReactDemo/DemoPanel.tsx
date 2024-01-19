f49b3ea8af20317e7baa67ae81149936f3947703
futugyou <17871902+futugyou@users.noreply.github.com>
2024-01-19 17:34:10 +0800

    update demo page layout

--- src/ReactDemo/DemoPanel.tsx
+++ src/ReactDemo/DemoPanel.tsx
@@ -2,23 +2,25 @@ import "./DemoPanel.css"
 
 import React from "react"
 
-import { useState, useEffect } from 'react';
-import { Outlet, NavLink, useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Outlet, NavLink, useLocation } from "react-router-dom"
 
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Popover from 'react-bootstrap/Popover';
-import { BsListUl } from "react-icons/bs";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
+import { BsListUl } from "react-icons/bs"
+
+import { DemoRoute } from '@/ReactDemo/DemoRoute'
 
 const DemoPanel = (props: any) => {
-    const [show, setShow] = useState(true);
-    const location = useLocation();
+    const [show, setShow] = useState(true)
+    const location = useLocation()
     useEffect(() => {
-        const pathname = location.pathname;
+        const pathname = location.pathname
         console.log(pathname)
         setShow(true)
-    }, [location]);
+    }, [location])
 
-    let popover = show ? "hide the menu" : "open the menu";
+    let popover = show ? "hide the menu" : "open the menu"
     const HandleShowIconClick = (e: any) => {
         setShow(s => !s)
     }
@@ -29,7 +31,27 @@ const DemoPanel = (props: any) => {
                 {popover}
             </Popover.Body>
         </Popover>
-    );
+    )
+
+    const liList = DemoRoute.children?.filter(p => p.path)
+        .filter(p => p.show && p.show() || !p.show).map(p => {
+            let href = p.href
+            if (!href) {
+                href = DemoRoute.path + "/" + p.path
+            }
+
+            let display = p.display
+            if (!display) {
+                display = p.path
+            }
+
+            return (
+                <li className="nav-item">
+                    <NavLink key={href} to={href} className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >{display}</NavLink>
+                </li>
+            )
+        })
+
     return (
         <>
             <OverlayTrigger placement="right" overlay={showPopover}>
@@ -41,30 +63,7 @@ const DemoPanel = (props: any) => {
                 {show && (
                     <div className="left-menu" >
                         <ul className="nav nav-pills flex-column mb-auto">
-                            <li className="nav-item">
-                                <NavLink to="/basic/app" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >App</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/game" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Game</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/form" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >NameForm</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/calculator" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Calculator</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/dialog" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >WelcomeDialog</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/split" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >SplitPaneApp</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/bailout" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >Bailout</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/basic/withbailout" className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""} >WithoutBailout</NavLink>
-                            </li>
+                            {liList}
                         </ul>
                     </div>
                 )}
