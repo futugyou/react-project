6ea3b527ecae75ae2656b55abaa4179c8384d683
futugyou <17871902+futugyou@users.noreply.github.com>
2024-03-08 20:05:19 +0800

    get company data from alphavantage server

--- src/Alphavantage/service.ts
+++ src/Alphavantage/service.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import axios, { AxiosRequestConfig } from 'axios'
 
-import { News } from './model'
+import { News, Company } from './model'
 
 const alphavantage_server = import.meta.env.REACT_APP_ALPHAVANTAGE
 
@@ -18,6 +18,29 @@ export const useAlphavantageNewsData = (ticker: string, config = {}) => {
     const { isLoading, isError, data, refetch, isFetching } = useQuery({
         queryKey: [keyPerfix + 'v1/news/' + ticker],
         queryFn: () => axios<News[]>(options).then(x => x.data),
+        ...config,
+    })
+
+    return {
+        data,
+        isLoading,
+        isFetching,
+        isError,
+        refetch,
+    }
+}
+
+export const useAlphavantageCompanyData = (config = {}) => {
+    const options: AxiosRequestConfig = {
+        url: alphavantage_server + 'v1/company',
+        method: "GET",
+        headers: {
+        },
+    }
+
+    const { isLoading, isError, data, refetch, isFetching } = useQuery({
+        queryKey: [keyPerfix + 'v1/company'],
+        queryFn: () => axios<Company[]>(options).then(x => x.data),
         ...config,
     })
 
