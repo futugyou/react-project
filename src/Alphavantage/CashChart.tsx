f2b63f1b76fed73a134972cfc4ac09342c3df970
futugyou <17871902+futugyou@users.noreply.github.com>
2024-03-10 04:12:18 +0800

    add cash chart

--- src/Alphavantage/CashChart.tsx
+++ src/Alphavantage/CashChart.tsx
@@ -8,7 +8,7 @@ import { AreaChartProps, Link, Select, SelectProps } from "@cloudscape-design/co
 import _, { isNaN } from 'lodash-es'
 import moment from 'moment'
 
-import { useBalanceData } from '@/Alphavantage/service'
+import { useCashData } from '@/Alphavantage/service'
 import EmptyChart from '@/Alphavantage/EmptyChart'
 import NoMatchChart from '@/Alphavantage/NoMatchChart'
 import Dropdown, { DropdownItem } from "@/Common/Dropdown"
@@ -23,42 +23,33 @@ const createRoutePath = (title: string, time: Date) => {
 }
 
 const dataTypes = [
-    { value: "TotalAssets", label: "Total Assets", },
-    { value: "TotalCurrentAssets", label: "Total Current Assets", },
-    { value: "CashAndCashEquivalentsAtCarryingValue", label: "Cash And Cash Equivalents At Carrying Value", },
-    { value: "CashAndShortTermInvestments", label: "Cash And Short Term Investments", },
-    { value: "Inventory", label: "Inventory", },
-    { value: "CurrentNetReceivables", label: "Current Net Receivables", },
-    { value: "TotalNonCurrentAssets", label: "Total Non Current Assets", },
-    { value: "PropertyPlantEquipment", label: "Property Plant Equipment", },
-    { value: "AccumulatedDepreciationAmortizationPPE", label: "Accumulated Depreciation Amortization PPE", },
-    { value: "IntangibleAssets", label: "Intangible Assets", },
-    { value: "IntangibleAssetsExcludingGoodwill", label: "Intangible Assets Excluding Goodwill", },
-    { value: "Goodwill", label: "Goodwill", },
-    { value: "Investments", label: "Investments", },
-    { value: "LongTermInvestments", label: "Long Term Investments", },
-    { value: "ShortTermInvestments", label: "Short Term Investments", },
-    { value: "OtherCurrentAssets", label: "Other Current Assets", },
-    { value: "OtherNonCurrentAssets", label: "Other Non CurrentAssets", },
-    { value: "TotalLiabilities", label: "Total Liabilities", },
-    { value: "TotalCurrentLiabilities", label: "Total Current Liabilities", },
-    { value: "CurrentAccountsPayable", label: "Current Accounts Payable", },
-    { value: "DeferredRevenue", label: "Deferred Revenue", },
-    { value: "CurrentDebt", label: "Current Debt", },
-    { value: "ShortTermDebt", label: "Short Term Debt", },
-    { value: "TotalNonCurrentLiabilities", label: "Total Non Current Liabilities", },
-    { value: "CapitalLeaseObligations", label: "Capital Lease Obligations", },
-    { value: "LongTermDebt", label: "Long Term Debt", },
-    { value: "CurrentLongTermDebt", label: "Current Long Term Debt", },
-    { value: "LongTermDebtNoncurrent", label: "Long Term Debt Noncurrent", },
-    { value: "ShortLongTermDebtTotal", label: "Short Long Term Debt Total", },
-    { value: "OtherCurrentLiabilities", label: "Other Current Liabilities", },
-    { value: "OtherNonCurrentLiabilities", label: "Other Non Current Liabilities", },
-    { value: "TotalShareholderEquity", label: "Total Shareholder Equity", },
-    { value: "TreasuryStock", label: "Treasury Stock", },
-    { value: "RetainedEarnings", label: "Retained Earnings", },
-    { value: "CommonStock", label: "Common Stock", },
-    { value: "CommonStockSharesOutstanding", label: "Common Stock Shares Outstanding", }
+    { label: "Operating Cashflow", value: "OperatingCashflow", },
+    { label: "Payments For Operating Activities", value: "PaymentsForOperatingActivities", },
+    { label: "Proceeds From Operating Activities", value: "ProceedsFromOperatingActivities", },
+    { label: "Change In Operating Liabilities", value: "ChangeInOperatingLiabilities", },
+    { label: "Change In Operating Assets", value: "ChangeInOperatingAssets", },
+    { label: "Depreciation Depletion And Amortization", value: "DepreciationDepletionAndAmortization", },
+    { label: "Capital Expenditures", value: "CapitalExpenditures", },
+    { label: "Change In Receivables", value: "ChangeInReceivables", },
+    { label: "Change In Inventory", value: "ChangeInInventory", },
+    { label: "Profit Loss", value: "ProfitLoss", },
+    { label: "Cashflow From Investment", value: "CashflowFromInvestment", },
+    { label: "Cashflow From Financing", value: "CashflowFromFinancing", },
+    { label: "Proceeds From Repayments Of Short Term Debt", value: "ProceedsFromRepaymentsOfShortTermDebt", },
+    { label: "Payments For Repurchase Of Common Stock", value: "PaymentsForRepurchaseOfCommonStock", },
+    { label: "Payments For Repurchase Of Equity", value: "PaymentsForRepurchaseOfEquity", },
+    { label: "Payments For Repurchase Of Preferred Stock", value: "PaymentsForRepurchaseOfPreferredStock", },
+    { label: "Dividend Payout", value: "DividendPayout", },
+    { label: "Dividend Payout Common Stock", value: "DividendPayoutCommonStock", },
+    { label: "Dividend Payout Preferred Stock", value: "DividendPayoutPreferredStock", },
+    { label: "Proceeds From Issuance Of Common Stock", value: "ProceedsFromIssuanceOfCommonStock", },
+    { label: "Proceeds From Issuance Of Long Term Debt And Capital Securities Net", value: "ProceedsFromIssuanceOfLongTermDebtAndCapitalSecuritiesNet", },
+    { label: "Proceeds From Issuance Of Preferred Stock", value: "ProceedsFromIssuanceOfPreferredStock", },
+    { label: "Proceeds From Repurchase Of Equity", value: "ProceedsFromRepurchaseOfEquity", },
+    { label: "Proceeds From Sale Of Treasury Stock", value: "ProceedsFromSaleOfTreasuryStock", },
+    { label: "Change In Cash And Cash Equivalents", value: "ChangeInCashAndCashEquivalents", },
+    { label: "Change In Exchange Rate", value: "ChangeInExchangeRate", },
+    { label: "Net Income", value: "NetIncome", },
 ]
 
 const dateGapTypes = [
@@ -66,10 +57,10 @@ const dateGapTypes = [
     { value: "Annual", label: "Annual", },
 ]
 
-const BalanceChart = () => {
-    const { data: nodeData, refetch: loadSelected, isLoading, isFetching, isError } = useBalanceData()
+const CashChart = () => {
+    const { data: nodeData, refetch: loadSelected, isLoading, isFetching, isError } = useCashData()
     const [Series, SetSeries] = useState<any[]>([])
-    const [selectedDataTypeOption, setSelectedDataTypeOption] = useState({ value: "TotalAssets", label: "TotalAssets", })
+    const [selectedDataTypeOption, setSelectedDataTypeOption] = useState({ value: "OperatingCashflow", label: "Operating Cash flow", })
     const [selectedDataGapTypeOption, setSelectedDataGapTypeOption] = useState({ value: "Quarterly", label: "Quarterly", })
 
     const HandleDataTypeChange = useCallback((event: NonCancelableCustomEvent<SelectProps.ChangeDetail>) => {
@@ -135,7 +126,7 @@ const BalanceChart = () => {
                 }
             }
             xScaleType="time"
-            yTitle={"Balance " + selectedDataGapTypeOption.label + " (" + selectedDataTypeOption.label + ") Data (USD)"}
+            yTitle={"Cash " + selectedDataGapTypeOption.label + " (" + selectedDataTypeOption.label + ") Data (USD)"}
             empty={Empty}
             noMatch={NoMatch}
             statusType={isLoading ? "loading" : "finished"}
@@ -153,7 +144,7 @@ const BalanceChart = () => {
                         <Select
                             options={dataTypes}
                             selectedOption={selectedDataTypeOption}
-                            placeholder="Balance Data Type"
+                            placeholder="Cash Data Type"
                             onChange={HandleDataTypeChange}
                         />
                     </div>
@@ -161,7 +152,7 @@ const BalanceChart = () => {
                         <Select
                             options={dateGapTypes}
                             selectedOption={selectedDataGapTypeOption}
-                            placeholder="Balance Data Gap Type"
+                            placeholder="Cash Data Gap Type"
                             onChange={HandleDataGapTypeChange}
                         />
                     </div>
@@ -173,4 +164,4 @@ const BalanceChart = () => {
     )
 }
 
-export default BalanceChart
+export default CashChart
--- src/Alphavantage/Route.tsx
+++ src/Alphavantage/Route.tsx
@@ -3,9 +3,14 @@ import { lazy } from 'react'
 import { RouteDescription } from '@/RouteDescription'
 
 const BalanceChart = lazy(() => import('./BalanceChart'))
+const CashChart = lazy(() => import('./CashChart'))
 
 export const ChatRoute: RouteDescription[] = [{
     path: "balance",
     display: "Balance",
     element: <BalanceChart />,
+}, {
+    path: "cash",
+    display: "Cash",
+    element: <CashChart />,
 }]
