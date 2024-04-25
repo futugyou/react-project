70b172e6ac9324ad724fda79ffdd0c6041988604
futugyou <17871902+futugyou@users.noreply.github.com>
2024-04-26 02:26:34 +0800

    remove eslint

--- vite.config.ts
+++ vite.config.ts
@@ -30,16 +30,5 @@ export default defineConfig(({ command, mode }) => {
     define: {
       'process.env': {}
     },
-    // build: {
-    //   rollupOptions: {
-    //     output: {
-    //       manualChunks(id) {
-    //         if (id.includes('node_modules')) {
-    //           return id.toString().split('node_modules/')[1].split('/')[0].toString()
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
   }
 })
