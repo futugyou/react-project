554ae4f37ba4a3a992959e29d1f132f18633010b
futugyou <17871902+futugyou@users.noreply.github.com>
2024-05-03 21:00:08 +0800

    update plate

--- src/Plate/PlateEditor.tsx
+++ src/Plate/PlateEditor.tsx
@@ -1,6 +1,7 @@
 
 import { Editor } from '@/Plate/components/plate-ui/editor'
-import { createPlugins, Plate } from '@udecode/plate-common'
+import { TooltipProvider } from '@radix-ui/react-tooltip'
+import { createPlugins, Plate, PlateContent } from '@udecode/plate-common'
 import { useMemo, useState } from 'react'
 import { initialValue } from './PlateInitValue'
 import { plugins, CustomPlugins } from './plugins'
@@ -8,20 +9,24 @@ import { createDemoPlugin } from './Plugins/Demo'
 
 const BasicEditor = () => {
     const [username, setNsername] = useState('')
-    const plugins = useMemo(
-        () =>
-            createPlugins([
-                createDemoPlugin({
-                    options: { username },
-                }),
-            ]),
-        [username]
-    )
+    // const plugins = useMemo(
+    //     () =>
+    //         createPlugins([
+    //             createDemoPlugin({
+    //                 options: { username },
+    //             }),
+    //         ]),
+    //     [username]
+    // )
 
     return (
-        <Plate initialValue={initialValue} plugins={plugins}>
-            <Editor spellCheck={false} autoFocus={false} placeholder="Type..." />
-        </Plate>
+        <div style={{ textAlign: 'left' }}>
+            <TooltipProvider>
+                <Plate plugins={plugins} initialValue={initialValue}>
+                    <PlateContent />
+                </Plate>
+            </TooltipProvider>
+        </div>
     )
 }
 
