c1a5026afce39a354e6ebbcf3d35cf60646623df
futugyou <17871902+futugyou@users.noreply.github.com>
2024-08-11 07:26:29 +0800

    update cros for react project

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  console.log(process.env.NODE_ENV)
-  
+  loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [react(),],
     envPrefix: "REACT_APP_",
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      }
-    },
-    server: {
-      cors: {
-        origin: true,
-        methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
-        allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "Authorization", "Access-Control-Allow-Headers", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "x-requested-with"],
-        exposedHeaders: ["Access-Control-Allow-Origin", "Origin", "Authorization", "Access-Control-Allow-Headers", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "x-requested-with"],
-        credentials: true
+        '@': '/src',
+        '~bootstrap': '/node_modules/bootstrap',
       }
     },
     define: {
