729a6fde6fb0217fcd2a8e23300dcdfdd8593e80
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-10-02 08:00:59 +0000

    Added `window` extension to support micro app

---
+++ src/@types/global.d.ts
@@ -0,0 +1,15 @@
+declare global {
+    interface Window {
+        microApp: any
+        __MICRO_APP_NAME__: string
+        __MICRO_APP_ENVIRONMENT__: string
+        __MICRO_APP_BASE_ROUTE__: string
+
+        [key: `micro-app-${string}`]: {
+            mount: () => void
+            unmount: () => void
+        }
+    }
+}
+
+export { }
--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import ReactDOM from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 
-import { QueryClientProvider, } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/Common/ReactQuery/client'
 
 import Loading from '@/Common/Components/Loading'
@@ -18,18 +18,40 @@ import { AuthProvider, authService } from '@/Auth/index'
 
 import { router } from '@/Route'
 
-ReactDOM.createRoot(document.getElementById('openai-web-root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<Loading />}>
-        <AuthProvider authService={authService} >
-          <ComponentProvider >
-            <RouterProvider router={router} />
-            <Analytics></Analytics>
-            <SpeedInsights></SpeedInsights>
-          </ComponentProvider>
-        </AuthProvider>
-      </Suspense>
-    </QueryClientProvider>
-  </React.StrictMode>,
-)
+let root: ReactDOM.Root | null = null
+
+function mount() {
+  const container = document.getElementById('openai-web-root') as HTMLElement
+  if (!container) throw new Error('Root container not found')
+
+  root = ReactDOM.createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={<Loading />}>
+          <AuthProvider authService={authService}>
+            <ComponentProvider>
+              <RouterProvider router={router} />
+              <Analytics />
+              <SpeedInsights />
+            </ComponentProvider>
+          </AuthProvider>
+        </Suspense>
+      </QueryClientProvider>
+    </React.StrictMode>
+  )
+}
+
+function unmount() {
+  if (root) {
+    root.unmount()
+    root = null
+    console.log('sub app react unmount!')
+  }
+}
+
+if (window.__MICRO_APP_ENVIRONMENT__) {
+  window[`micro-app-${window.__MICRO_APP_NAME__}`] = { mount, unmount }
+} else {
+  mount()
+}
