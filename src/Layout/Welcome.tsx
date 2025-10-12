35aba01c4520841c73317122e05a848bd8718a30
futugyou <17871902+futugyou@users.noreply.github.com>
2025-10-12 19:57:07 +0000

    Updated welcome use useHref to get the url

--- src/Layout/Welcome.tsx
+++ src/Layout/Welcome.tsx
@@ -1,67 +1,75 @@
 
 import './Welcome.css'
 
+import { useHref } from "react-router-dom"
 import { Link } from '@cloudscape-design/components'
+
+import { patchWindowOpen } from '@/MicroApp/event'
 
 const Welcome = () => {
     const items = [
         {
             title: "company news",
-            url: "./e/news",
+            url: "/e/news",
             image: "./welcome/news.png",
             description: "",
         },
         {
             title: "economic indicators",
-            url: "./e/economic",
+            url: "/e/economic",
             image: "./welcome/economic.png",
             description: "",
         },
         {
             title: "commodities",
-            url: "./e/commodities",
+            url: "/e/commodities",
             image: "./welcome/commodities.png",
             description: "",
         },
         {
             title: "stock series",
-            url: "./e/stockSeries?symbol=IBM",
+            url: "/e/stockSeries?symbol=IBM",
             image: "./welcome/stock.png",
             description: "",
         },
         {
             title: "fundamenta",
-            url: "./e/balance",
+            url: "/e/balance",
             image: "./welcome/fundamenta.png",
             description: "",
         },
         {
             title: "excalidraw",
-            url: "./w/excalidraw",
+            url: "/w/excalidraw",
             image: "./welcome/excalidraw.png",
             description: "",
         },
         {
             title: "aws resource",
-            url: "./flow/cytoscape",
+            url: "/flow/cytoscape",
             image: "./welcome/awsresource.png",
             description: "",
         },
         {
             title: ".net flow",
-            url: "./flow/dotnet/di",
+            url: "/flow/dotnet/di",
             image: "./welcome/netflow.png",
             description: "",
         },
         {
             title: "bookshelf",
-            url: "./bookshelf",
+            url: "/bookshelf",
             image: "./welcome/bookshelf.png",
             description: "",
         }
     ]
 
+    const openExternalUrl = (url: string) => {
+        patchWindowOpen(url)
+    }
+
     const welcomeitem = items.map((i, index) => {
+        const href = useHref(i.url)
         return (
             <div key={i.title} className='welcome-item'>
                 <img
@@ -70,7 +78,8 @@ const Welcome = () => {
                     alt={i.title}
                 />
                 <div className="welcome-desc">
-                    <Link href={i.url} external={true} fontSize="heading-m">{i.title}</Link>
+                    <Link onFollow={() => openExternalUrl(href)}
+                        external={true} fontSize="heading-m">{i.title}</Link>
                 </div>
             </div>
         )
--- src/MicroApp/event.ts
+++ src/MicroApp/event.ts
@@ -17,3 +17,36 @@ export const handleMicroData = () => {
         }, 3000)
     }
 }
+
+export const patchWindowOpen = (url: string) => {
+    // window.rawWindow is main app window, window is sub app window
+    let win = (window.rawWindow ?? window) as Window & typeof globalThis
+    // let win = window
+
+    if (url.startsWith('http://') || url.startsWith('https://')) {
+        win.open(url, '_blank')
+    }
+
+    if (window.__MICRO_APP_ENVIRONMENT__) {
+        // in a micro-frontend environment, window.rawWindow must exist and represent the main window.
+        // use main app window to get location info
+        // for exmaple: main url is `https://main.testing.app/reactapp?reactapp=%2Freact%2Fproject`, sub app url is  `https://sub.react.app/react/project`
+        // main: 
+        //      origin: https://main.testing.app
+        //      path: /reactapp
+        // sub:
+        //      origin: https://sub.react.app
+        //      path: /react/project        
+        // i want get /reactapp, NOT /react/project
+        let origin = win.location.origin.replace(/\/+$/, '')
+        let path = win.location.pathname.replace(/^\/+/, '')
+
+        let finalUrl = origin + '/' + path + '?' + path + '=' + url
+
+        console.log('window open url is:', finalUrl);
+
+        win.open(finalUrl, '_blank')
+    } else {
+        win.open(url, '_blank')
+    }
+}
