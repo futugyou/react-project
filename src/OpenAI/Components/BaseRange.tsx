547d913272feb3969f0063115b08d47781dec735
futugyou <17871902+futugyou@users.noreply.github.com>
2025-10-13 18:28:05 +0000

    Updated baserange component use cloudscape-design instead of react-bootstrap

--- src/OpenAI/Components/BaseRange.tsx
+++ src/OpenAI/Components/BaseRange.tsx
@@ -1,11 +1,14 @@
+
 import { useState, useEffect } from "react"
-
-import Form from 'react-bootstrap/Form'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Popover from 'react-bootstrap/Popover'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
+import {
+    Box,
+    ColumnLayout,
+    FormField,
+    Input,
+    Slider,
+    Popover,
+    SpaceBetween,
+} from "@cloudscape-design/components"
 
 interface IBaseRangeProps {
     value: number
@@ -27,93 +30,78 @@ const BaseRange = (props: IBaseRangeProps) => {
         value = props.min
     }
 
-    let rawstring = value + ''
-    const [stringValue, setStringValue] = useState<string>(rawstring)
-
-    const modelPopover = (
-        <Popover id="model-popover">
-            <Popover.Body>
-                {props.popover}
-            </Popover.Body>
-        </Popover>
-    )
+    const [stringValue, setStringValue] = useState<string>(value.toString())
 
     const handleInputChange = (value: string) => {
-        if (value === '') {
+        if (value === "") {
             setStringValue("0")
             return
         }
 
-        if (value === '.') {
+        if (value === ".") {
             setStringValue("0.")
             return
         }
 
-        if (value.charAt(value.length - 1) === '.') {
+        if (value.charAt(value.length - 1) === ".") {
             setStringValue(value)
             return
         }
 
-        const t = parseFloat(value) + ''
-        setStringValue(t)
+        setStringValue(parseFloat(value).toString())
     }
 
     const handleValueChange = (value: string) => {
         setStringValue(value)
     }
 
-    const handleChange = () => {
-        // if (props.onValueChange) {
-        //     props.onValueChange(t)
-        // }
-    }
-
     useEffect(() => {
-        if (rawstring != stringValue) {
-            const timeOutId = setTimeout(() => {
-                const t = parseFloat(stringValue) + ''
-                if (props.onValueChange) {
-                    props.onValueChange(t)
-                    rawstring = new Date().toDateString()
-                }
-            }, 500)
-            return () => clearTimeout(timeOutId)
-        }
+        const timeOutId = setTimeout(() => {
+            const t = parseFloat(stringValue).toString()
+            if (props.onValueChange) {
+                props.onValueChange(t)
+            }
+        }, 500)
+        return () => clearTimeout(timeOutId)
     }, [stringValue])
 
-    const renderRange = () => {
-        return <Form.Group className="mb-3" >
-            <Row>
-                <Col>
-                    <Form.Label>{props.display}</Form.Label>
-                </Col>
-                <Col xs="4">
-                    <Form.Control value={stringValue} className="display-value" onChange={e => handleInputChange(e.target.value)} />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Form.Range min={props.min} max={props.max} step={props.step} value={stringValue} onMouseUp={handleChange} onChange={e => handleValueChange(e.target.value)} />
-                </Col>
-            </Row>
-        </Form.Group>
-    }
+    const renderRange = () => (
+        <SpaceBetween size="s">
+            <ColumnLayout columns={2} variant="text-grid">
+                <FormField label={props.display}>
+                    <Input
+                        value={stringValue}
+                        onChange={(e) => handleInputChange(e.detail.value)}
+                    />
+                </FormField>
+            </ColumnLayout>
+            <FormField>
+                <Slider
+                    min={props.min}
+                    max={props.max}
+                    step={props.step}
+                    value={[parseFloat(stringValue)]}
+                    onChange={(e) => handleValueChange(e.detail.value)}
+                />
+            </FormField>
+        </SpaceBetween>
+    )
 
     if (props.disablePopover) {
-        return (
-            renderRange()
-        )
+        return renderRange()
     }
 
     return (
-        <>
-            {/* <OverlayTrigger placement="left" overlay={modelPopover} delay={{ show: 100, hide: 1000 }} > */}
-            <OverlayTrigger placement="left" overlay={modelPopover}>
-                {renderRange()}
-            </OverlayTrigger>
-        </>
+        <Popover
+            size="small"
+            position="right"
+            dismissButton={false}
+            triggerType="custom"
+            content={props.popover}
+        >
+            {renderRange()}
+        </Popover>
     )
 }
 
 export default BaseRange
-
