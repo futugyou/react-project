ee4a142a2bfcddee49bacf981895137c65fbe98a
futugyou <17871902+futugyou@users.noreply.github.com>
2025-10-13 18:27:45 +0000

    Updated dropdown component use cloudscape-design instead of react-bootstrap

--- src/Common/Components/Dropdown.tsx
+++ src/Common/Components/Dropdown.tsx
@@ -1,10 +1,7 @@
-import './Dropdown.css'
-
+import "./Dropdown.css"
 import { useState } from "react"
 import { BsChevronDown } from "react-icons/bs"
-
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Popover from 'react-bootstrap/Popover'
+import Popover from "@cloudscape-design/components/popover"
 
 export interface DropdownItem {
     key: string
@@ -15,37 +12,29 @@ export interface DropdownItem {
 
 interface DropdownProps {
     items: DropdownItem[]
-    onDropdownChange?: (key: string) => void;
+    onDropdownChange?: (key: string) => void
     children?: React.ReactNode
 }
 
 const Dropdown = (props: DropdownProps) => {
     if (props.items.length < 1) {
-        return (<></>)
+        return <></>
     }
 
     const [showDropdown, setShowDropdown] = useState(false)
     const [emptyDisplay, setEmptyDisplay] = useState(props.items[0].value)
     const ulClassName = showDropdown ? "dropdown-menu show" : "dropdown-menu"
 
-    const itemPopover = (item: string) => (
-        <Popover id={item + "-popover"}>
-            <Popover.Body>
-                {item}
-            </Popover.Body>
-        </Popover>
-    )
-
-    const HandleContainerClick = (e: any) => {
-        setShowDropdown(f => !f)
+    const HandleContainerClick = () => {
+        setShowDropdown((f) => !f)
     }
 
-    const HandleContainerBlur = (e: any) => {
+    const HandleContainerBlur = () => {
         setShowDropdown(false)
     }
 
     const HandleDropdownClick = (key: string) => {
-        const value = props.items.find(i => i.key === key)!.value
+        const value = props.items.find((i) => i.key === key)!.value
         setEmptyDisplay(value)
 
         if (props.onDropdownChange) {
@@ -53,33 +42,46 @@ const Dropdown = (props: DropdownProps) => {
         }
     }
 
-    const dropdownitems = props.items.map(i => {
-        if (!i.popover) {
-            return (
-                <li key={i.key} className={i.value == emptyDisplay ? "dropdown-item selected" : "dropdown-item"} onClick={() => HandleDropdownClick(i.key)}> {i.value} </li>
-            )
-        }
+    const dropdownitems = props.items.map((i) => {
+        const itemNode = (
+            <li
+                key={i.key}
+                className={i.value === emptyDisplay ? "dropdown-item selected" : "dropdown-item"}
+                onClick={() => HandleDropdownClick(i.key)}
+            >
+                {i.value}
+            </li>
+        )
+
+        if (!i.popover) return itemNode
 
         return (
-            <OverlayTrigger key={i.key} placement="left" overlay={itemPopover(i.popover)}>
-                <li className={i.value == emptyDisplay ? "dropdown-item selected" : "dropdown-item"} onClick={() => HandleDropdownClick(i.key)}> {i.value} </li>
-            </OverlayTrigger>
+            <Popover
+                key={i.key}
+                dismissButton={false}
+                content={<div>{i.popover}</div>}
+                position="left"
+                triggerType="hover"
+                size="small"
+            >
+                {itemNode}
+            </Popover>
         )
     })
 
     return (
-        <div className="dropdown-container" onClick={HandleContainerClick} onBlur={HandleContainerBlur}>
+        <div
+            className="dropdown-container"
+            onClick={HandleContainerClick}
+            onBlur={HandleContainerBlur}
+        >
             <div className="dropdown-display-container">
-                <div className="dropdown-display" >
-                    {emptyDisplay}
-                </div>
+                <div className="dropdown-display">{emptyDisplay}</div>
                 <div className="dropdown-icon">
-                    <BsChevronDown></BsChevronDown>
+                    <BsChevronDown />
                 </div>
             </div>
-            <ul className={ulClassName}>
-                {dropdownitems}
-            </ul>
+            <ul className={ulClassName}>{dropdownitems}</ul>
         </div>
     )
 }
--- src/Layout/HeaderMenu.tsx
+++ src/Layout/HeaderMenu.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { useNavigate, useLocation } from "react-router-dom"
 import TopNavigation from "@cloudscape-design/components/top-navigation"
-import SideNavigation from "@cloudscape-design/components/side-navigation"
 import type { ButtonDropdownProps } from '@cloudscape-design/components'
 import { RouteDescription } from "@/RouteDescription"
 
